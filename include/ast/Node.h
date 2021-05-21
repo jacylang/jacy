846dc67bed912250f5b95720a424ae1fbdc69369
is_gordeev <gordeev.john@gmail.com>
2021-05-22 00:33:57 +0300

    `NodeMap`

--- include/ast/Node.h
+++ include/ast/Node.h
@@ -10,11 +10,34 @@ namespace jc::ast {
     struct Node;
     using span::Span;
     using node_ptr = std::shared_ptr<Node>;
+    using node_id = uint32_t;
+
+    class NodeMap {
+    public:
+        NodeMap() = default;
+
+        void addNode(Node * node) {
+            lastNodeId++;
+            nodes.emplace(lastNodeId, node);
+        }
+
+        const Node & getNode(node_id nodeId) const {
+            return *nodes.at(nodeId);
+        }
+
+    private:
+        node_id lastNodeId{0};
+        std::map<node_id, Node*> nodes;
+    };
 
     struct Node {
-        explicit Node(const Span & span) : span(span) {}
+        explicit Node(const Span & span) : span(span) {
+            nodeMap.addNode(this);
+        }
 
         Span span;
+
+        static NodeMap nodeMap;
     };
 }
 
