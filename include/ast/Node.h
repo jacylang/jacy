6b4d5a2f379fcda7c8d787420747630c3fa37d38
is_gordeev <gordeev.john@gmail.com>
2021-05-22 00:37:29 +0300

    Assign each `Node` unique `node_id`

--- include/ast/Node.h
+++ include/ast/Node.h
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <utility>
+#include <cstdint>
 
 #include "parser/Token.h"
 
@@ -12,13 +13,18 @@ namespace jc::ast {
     using node_ptr = std::shared_ptr<Node>;
     using node_id = uint32_t;
 
+    const node_id NONE_NODE_ID = UINT32_MAX;
+
     class NodeMap {
     public:
         NodeMap() = default;
 
-        void addNode(Node * node) {
-            lastNodeId++;
-            nodes.emplace(lastNodeId, node);
+        node_id addNode(Node * node) {
+            if (currentNodeId == NONE_NODE_ID) {
+                common::Logger::devPanic("Nodes count exceeded");
+            }
+            nodes.emplace(currentNodeId, node);
+            return currentNodeId++;
         }
 
         const Node & getNode(node_id nodeId) const {
@@ -26,15 +32,16 @@ namespace jc::ast {
         }
 
     private:
-        node_id lastNodeId{0};
+        node_id currentNodeId{0};
         std::map<node_id, Node*> nodes;
     };
 
     struct Node {
         explicit Node(const Span & span) : span(span) {
-            nodeMap.addNode(this);
+            id = nodeMap.addNode(this);
         }
 
+        node_id id{NONE_NODE_ID};
         Span span;
 
         static NodeMap nodeMap;
