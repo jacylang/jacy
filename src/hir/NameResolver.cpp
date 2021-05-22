4b29e8f266f27a06eeecab7ccb59e8d70e944eef
is_gordeev <gordeev.john@gmail.com>
2021-05-22 12:58:21 +0300

    Save updated `NameResolver`

--- src/hir/NameResolver.cpp
+++ src/hir/NameResolver.cpp
@@ -1,10 +1,16 @@
 #include "hir/NameResolver.h"
 
 namespace jc::hir {
+    // Statements //
+    void NameResolver::visit(ast::ExprStmt * exprStmt) {
+        exprStmt->expr->accept(*this);
+    }
+
     void NameResolver::visit(ast::Item * item) {
         item->stmt->accept(*this);
     }
 
+    // Expressions //
     void NameResolver::visit(ast::Block * block) {
         enterRib();
         for (const auto & stmt : block->stmts) {
@@ -13,6 +19,7 @@ namespace jc::hir {
         exitRib();
     }
 
+    // Ribs //
     void NameResolver::enterRib() {
         ribs.push(std::make_unique<Rib>());
         ribIndex = ribs.size() - 1;
@@ -24,4 +31,6 @@ namespace jc::hir {
         }
         ribIndex--;
     }
+
+    // Resolution //
 }
