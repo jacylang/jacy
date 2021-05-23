91200db11b552e74204a11129e9db01e7bf5e5ec
is_gordeev <gordeev.john@gmail.com>
2021-05-23 16:10:29 +0300

    NameResolver refactor pointer access to ref access

--- src/resolve/NameResolver.cpp
+++ src/resolve/NameResolver.cpp
@@ -7,181 +7,181 @@ namespace jc::resolve {
         return {rib, std::move(suggestions)};
     }
 
-    void NameResolver::visit(ast::FuncDecl * funcDecl) {
+    void NameResolver::visit(ast::FuncDecl & funcDecl) {
         uint32_t prevDepth = getDepth();
-        visitTypeParams(funcDecl->typeParams);
+        visitTypeParams(funcDecl.typeParams);
 
         enterRib(); // -> (signature rib)
-        for (const auto & param : funcDecl->params) {
+        for (const auto & param : funcDecl.params) {
             // TODO: Resolve params types
         }
         // TODO: Resolve return type
 
         enterRib(); // -> (params rib)
-        for (const auto & param : funcDecl->params) {
+        for (const auto & param : funcDecl.params) {
             declare(param->name->unwrapValue(), Name::Kind::Param, param->id);
         }
 
-        if (funcDecl->oneLineBody) {
-            funcDecl->oneLineBody.unwrap()->accept(*this);
+        if (funcDecl.oneLineBody) {
+            funcDecl.oneLineBody.unwrap()->accept(*this);
         } else {
-            funcDecl->body.unwrap()->accept(*this);
+            funcDecl.body.unwrap()->accept(*this);
         }
 
         liftToDepth(prevDepth); // <- (params rib) <- (all type params ribs)
     }
 
-    void NameResolver::visit(ast::VarStmt * varDecl) {
+    void NameResolver::visit(ast::VarStmt & varDecl) {
         // TODO: Split VarStmt and Field
         enterRib();
     }
 
     // Statements //
-    void NameResolver::visit(ast::ExprStmt * exprStmt) {
-        exprStmt->expr->accept(*this);
+    void NameResolver::visit(ast::ExprStmt & exprStmt) {
+        exprStmt.expr->accept(*this);
     }
 
-    void NameResolver::visit(ast::WhileStmt * whileStmt) {
-        whileStmt->condition->accept(*this);
-        visit(whileStmt->body.get());
+    void NameResolver::visit(ast::WhileStmt & whileStmt) {
+        whileStmt.condition->accept(*this);
+        visit(*whileStmt.body);
     }
 
     // Expressions //
-    void NameResolver::visit(ast::Assignment * assign) {
-        assign->lhs->accept(*this);
-        assign->rhs->accept(*this);
+    void NameResolver::visit(ast::Assignment & assign) {
+        assign.lhs->accept(*this);
+        assign.rhs->accept(*this);
     }
 
-    void NameResolver::visit(ast::Block * block) {
+    void NameResolver::visit(ast::Block & block) {
         enterRib(); // -> block rib
-        for (const auto & stmt : block->stmts) {
+        for (const auto & stmt : block.stmts) {
             stmt->accept(*this);
         }
         exitRib(); // <- block rib
     }
 
-    void NameResolver::visit(ast::BorrowExpr * borrowExpr) {
-        borrowExpr->expr->accept(*this);
+    void NameResolver::visit(ast::BorrowExpr & borrowExpr) {
+        borrowExpr.expr->accept(*this);
     }
 
-    void NameResolver::visit(ast::BreakExpr * breakExpr) {
-        if (breakExpr->expr) {
-            breakExpr->expr.unwrap()->accept(*this);
+    void NameResolver::visit(ast::BreakExpr & breakExpr) {
+        if (breakExpr.expr) {
+            breakExpr.expr.unwrap()->accept(*this);
         }
     }
 
-    void NameResolver::visit(ast::ContinueExpr * continueExpr) {}
+    void NameResolver::visit(ast::ContinueExpr & continueExpr) {}
 
-    void NameResolver::visit(ast::DerefExpr * derefExpr) {
-        derefExpr->expr->accept(*this);
+    void NameResolver::visit(ast::DerefExpr & derefExpr) {
+        derefExpr.expr->accept(*this);
     }
 
-    void NameResolver::visit(ast::IfExpr * ifExpr) {
-        ifExpr->condition->accept(*this);
-        if (ifExpr->ifBranch) {
-            ifExpr->ifBranch.unwrap()->accept(*this);
+    void NameResolver::visit(ast::IfExpr & ifExpr) {
+        ifExpr.condition->accept(*this);
+        if (ifExpr.ifBranch) {
+            ifExpr.ifBranch.unwrap()->accept(*this);
         }
-        if (ifExpr->elseBranch) {
-            ifExpr->elseBranch.unwrap()->accept(*this);
+        if (ifExpr.elseBranch) {
+            ifExpr.elseBranch.unwrap()->accept(*this);
         }
     }
 
-    void NameResolver::visit(ast::Infix * infix) {
-        infix->lhs->accept(*this);
-        infix->rhs->accept(*this);
+    void NameResolver::visit(ast::Infix & infix) {
+        infix.lhs->accept(*this);
+        infix.rhs->accept(*this);
     }
 
-    void NameResolver::visit(ast::Invoke * invoke) {
-        invoke->lhs->accept(*this);
-        visitNamedList(invoke->args);
+    void NameResolver::visit(ast::Invoke & invoke) {
+        invoke.lhs->accept(*this);
+        visitNamedList(invoke.args);
     }
 
-    void NameResolver::visit(ast::Lambda * lambdaExpr) {
+    void NameResolver::visit(ast::Lambda & lambdaExpr) {
         enterRib(); // -> (lambda params)
 
-        for (const auto & param : lambdaExpr->params) {
+        for (const auto & param : lambdaExpr.params) {
             // TODO
-//            param->name->accept(*this);
+//            param.name->accept(*this);
             if (param->type) {
                 param->type.unwrap()->accept(*this);
             }
         }
 
-        if (lambdaExpr->returnType) {
-            lambdaExpr->returnType.unwrap()->accept(*this);
+        if (lambdaExpr.returnType) {
+            lambdaExpr.returnType.unwrap()->accept(*this);
         }
 
-        lambdaExpr->body->accept(*this);
+        lambdaExpr.body->accept(*this);
 
         exitRib(); // <- (lambda params)
     }
 
-    void NameResolver::visit(ast::ListExpr * listExpr) {
-        for (const auto & el : listExpr->elements) {
+    void NameResolver::visit(ast::ListExpr & listExpr) {
+        for (const auto & el : listExpr.elements) {
             el->accept(*this);
         }
     }
 
-    void NameResolver::visit(ast::LiteralConstant * literalConstant) {}
+    void NameResolver::visit(ast::LiteralConstant & literalConstant) {}
 
-    void NameResolver::visit(ast::LoopExpr * loopExpr) {
-        visit(loopExpr->body.get());
+    void NameResolver::visit(ast::LoopExpr & loopExpr) {
+        visit(*loopExpr.body);
     }
 
-    void NameResolver::visit(ast::MemberAccess * memberAccess) {
-        memberAccess->lhs->accept(*this);
+    void NameResolver::visit(ast::MemberAccess & memberAccess) {
+        memberAccess.lhs->accept(*this);
 
         // TODO!!!: Resolve field
     }
 
-    void NameResolver::visit(ast::ParenExpr * parenExpr) {
-        parenExpr->expr->accept(*this);
+    void NameResolver::visit(ast::ParenExpr & parenExpr) {
+        parenExpr.expr->accept(*this);
     }
 
-    void NameResolver::visit(ast::PathExpr * pathExpr) {
+    void NameResolver::visit(ast::PathExpr & pathExpr) {
         // TODO!!!
     }
 
-    void NameResolver::visit(ast::Prefix * prefix) {
-        prefix->rhs->accept(*this);
+    void NameResolver::visit(ast::Prefix & prefix) {
+        prefix.rhs->accept(*this);
     }
 
-    void NameResolver::visit(ast::QuestExpr * questExpr) {
-        questExpr->expr->accept(*this);
+    void NameResolver::visit(ast::QuestExpr & questExpr) {
+        questExpr.expr->accept(*this);
     }
 
-    void NameResolver::visit(ast::ReturnExpr * returnExpr) {
-        if (returnExpr->expr) {
-            returnExpr->expr.unwrap()->accept(*this);
+    void NameResolver::visit(ast::ReturnExpr & returnExpr) {
+        if (returnExpr.expr) {
+            returnExpr.expr.unwrap()->accept(*this);
         }
     }
 
-    void NameResolver::visit(ast::SpreadExpr * spreadExpr) {
-        spreadExpr->expr->accept(*this);
+    void NameResolver::visit(ast::SpreadExpr & spreadExpr) {
+        spreadExpr.expr->accept(*this);
     }
 
-    void NameResolver::visit(ast::Subscript * subscript) {
-        subscript->lhs->accept(*this);
+    void NameResolver::visit(ast::Subscript & subscript) {
+        subscript.lhs->accept(*this);
 
-        for (const auto & index : subscript->indices) {
+        for (const auto & index : subscript.indices) {
             index->accept(*this);
         }
     }
 
-    void NameResolver::visit(ast::ThisExpr * thisExpr) {}
+    void NameResolver::visit(ast::ThisExpr & thisExpr) {}
 
-    void NameResolver::visit(ast::TupleExpr * tupleExpr) {
-        visitNamedList(tupleExpr->elements);
+    void NameResolver::visit(ast::TupleExpr & tupleExpr) {
+        visitNamedList(tupleExpr.elements);
     }
 
-    void NameResolver::visit(ast::UnitExpr * unitExpr) {
+    void NameResolver::visit(ast::UnitExpr & unitExpr) {
         // TODO MEOW?
     }
 
-    void NameResolver::visit(ast::WhenExpr * whenExpr) {
-        whenExpr->subject->accept(*this);
+    void NameResolver::visit(ast::WhenExpr & whenExpr) {
+        whenExpr.subject->accept(*this);
 
-        for (const auto & entry : whenExpr->entries) {
+        for (const auto & entry : whenExpr.entries) {
             enterRib(); // -> (when entry)
             for (const auto & cond : entry->conditions) {
                 cond->accept(*this);
@@ -192,39 +192,39 @@ namespace jc::resolve {
     }
 
     // Types //
-    void NameResolver::visit(ast::ParenType * parenType) {
-        parenType->type->accept(*this);
+    void NameResolver::visit(ast::ParenType & parenType) {
+        parenType.type->accept(*this);
     }
 
-    void NameResolver::visit(ast::TupleType * tupleType) {
-        for (const auto & el : tupleType->elements) {
+    void NameResolver::visit(ast::TupleType & tupleType) {
+        for (const auto & el : tupleType.elements) {
             if (el->type) {
                 el->type.unwrap()->accept(*this);
             }
         }
     }
 
-    void NameResolver::visit(ast::FuncType * funcType) {
-        for (const auto & param : funcType->params) {
+    void NameResolver::visit(ast::FuncType & funcType) {
+        for (const auto & param : funcType.params) {
             param->accept(*this);
         }
-        funcType->returnType->accept(*this);
+        funcType.returnType->accept(*this);
     }
 
-    void NameResolver::visit(ast::SliceType * listType) {
-        listType->type->accept(*this);
+    void NameResolver::visit(ast::SliceType & listType) {
+        listType.type->accept(*this);
     }
 
-    void NameResolver::visit(ast::ArrayType * arrayType) {
-        arrayType->type->accept(*this);
-        arrayType->sizeExpr->accept(*this);
+    void NameResolver::visit(ast::ArrayType & arrayType) {
+        arrayType.type->accept(*this);
+        arrayType.sizeExpr->accept(*this);
     }
 
-    void NameResolver::visit(ast::TypePath * typePath) {
+    void NameResolver::visit(ast::TypePath & typePath) {
         // TODO: !!!
     }
 
-    void NameResolver::visit(ast::UnitType * unitType) {
+    void NameResolver::visit(ast::UnitType & unitType) {
         // TODO: MEOW?
     }
 
