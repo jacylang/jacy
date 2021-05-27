58313b25dab19b5d8ced344afe5222c098944ef0
is_gordeev <gordeev.john@gmail.com>
2021-05-27 20:49:08 +0300

    Enum node structure update

--- include/ast/item/Enum.h
+++ include/ast/item/Enum.h
@@ -2,18 +2,40 @@
 #define JACY_AST_ITEM_ENUM_H
 
 #include <vector>
+#include <variant>
 
 #include "ast/item/Item.h"
 #include "ast/fragments/Identifier.h"
+#include "ast/fragments/Field.h"
 
 namespace jc::ast {
     struct EnumEntry;
     using enum_entry_ptr = std::shared_ptr<EnumEntry>;
     using enum_entry_list = std::vector<enum_entry_ptr>;
 
-    struct EnumEntry {
+    enum class EnumEntryKind {
+        Raw,
+        Discriminant,
+        Tuple,
+        Struct,
+    };
+
+    struct EnumEntry : Node {
+        EnumEntry(EnumEntryKind kind, id_ptr name, const Span & span)
+            : kind(kind), name(std::move(name)), body(std::monostate{}), Node(span) {}
+
+        EnumEntry(EnumEntryKind kind, id_ptr name, expr_ptr discriminant, const Span & span)
+            : kind(kind), name(std::move(name)), body(std::move(discriminant)), Node(span) {}
+
+        EnumEntry(EnumEntryKind kind, id_ptr name, named_list_ptr tupleFields, const Span & span)
+            : kind(kind), name(std::move(name)), body(std::move(tupleFields)), Node(span) {}
+
+        EnumEntry(EnumEntryKind kind, id_ptr name, field_list fields, const Span & span)
+            : kind(kind), name(std::move(name)), body(std::move(fields)), Node(span) {}
+
+        EnumEntryKind kind;
         id_ptr name;
-        expr_ptr value;
+        std::variant<std::monostate, expr_ptr, named_list_ptr, field_list> body;
     };
 
     struct Enum : Item {
