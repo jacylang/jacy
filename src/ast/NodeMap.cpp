808d59a02068c6af36c0c736979869f41219dc0d
is_gordeev <gordeev.john@gmail.com>
2021-05-27 14:25:21 +0300

    NodeMap add implementations

---
+++ src/ast/NodeMap.cpp
@@ -0,0 +1,15 @@
+#include "ast/NodeMap.h"
+
+namespace jc::ast {
+    const Node & NodeMap::getNode(node_id nodeId) const {
+        return *nodes.at(nodeId);
+    }
+
+    const Span & NodeMap::getNodeSpan(node_id nodeId) const {
+        return getNode(nodeId).span;
+    }
+
+    node_ptr NodeMap::getNodePtr(node_id nodeId) const {
+        return nodes.at(nodeId);
+    }
+}
