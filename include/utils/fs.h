9276a157fa573c79d65cb6292d4fada3a9b823b2
is_gordeev <gordeev.john@gmail.com>
2021-05-27 14:16:38 +0300

    Fix `utils::fs`

--- include/utils/fs.h
+++ include/utils/fs.h
@@ -53,52 +53,13 @@ namespace jc::utils::fs {
      * @param path
      * @return
      */
-    bool exists(const std_fs::path & path) {
-        return std_fs::exists(std_fs::relative(path));
-    }
+    bool exists(const std_fs::path & path);
 
-    entry_ptr readfile(const std_fs::path & path) {
-        if (not fs::exists(path)) {
-            common::Logger::devPanic("Called `fs::readfile` with non-existent file");
-        }
+    entry_ptr readfile(const std_fs::path & path);
 
-        const auto & entry = std_fs::directory_entry(path);
-        std::fstream file(path);
+    entry_list readdirRecEntries(const std_fs::path & path, const std::string & allowedExt = "");
 
-        if (!file.is_open()) {
-            common::Logger::devPanic("File " + path.string() + " not found");
-        }
-
-        std::stringstream ss;
-        ss << file.rdbuf();
-        auto data = ss.str();
-        file.close();
-
-        return std::make_shared<Entry>(path.string(), entry.file_size(), std::move(data));
-    }
-
-    entry_list readdirRecEntries(const std_fs::path & path, const std::string & allowedExt = "") {
-        entry_list entries;
-        for (const auto & entry : std_fs::directory_iterator(path)) {
-            const auto & entryPath = std_fs::relative(entry.path());
-            if (entry.is_directory()) {
-                entries.emplace_back(
-                    std::make_shared<Entry>(entryPath.string(), std::move(readdirRecEntries(entryPath)))
-                );
-            } else if (entry.is_regular_file()) {
-                if (entryPath.extension() != allowedExt) {
-                    continue;
-                }
-
-                entries.emplace_back(readfile(entryPath));
-            }
-        }
-        return std::move(entries);
-    }
-
-    entry_ptr readDirRec(const std_fs::path & path, const std::string & allowedExt = "") {
-        return std::make_shared<Entry>(path, readdirRecEntries(path, allowedExt));
-    }
+    entry_ptr readDirRec(const std_fs::path & path, const std::string & allowedExt = "");
 }
 
 #endif // JACY_UTILS_HASH_H
---
+++ src/utils/fs.cpp
@@ -0,0 +1,50 @@
+#include "utils/fs.h"
+
+namespace jc::utils::fs {
+    bool exists(const std_fs::path & path) {
+        return std_fs::exists(std_fs::relative(path));
+    }
+
+    entry_ptr readfile(const std_fs::path & path) {
+        if (not fs::exists(path)) {
+            common::Logger::devPanic("Called `fs::readfile` with non-existent file");
+        }
+
+        const auto & entry = std_fs::directory_entry(path);
+        std::fstream file(path);
+
+        if (!file.is_open()) {
+            common::Logger::devPanic("File " + path.string() + " not found");
+        }
+
+        std::stringstream ss;
+        ss << file.rdbuf();
+        auto data = ss.str();
+        file.close();
+
+        return std::make_shared<Entry>(path.string(), entry.file_size(), std::move(data));
+    }
+
+    entry_list readdirRecEntries(const std_fs::path & path, const std::string & allowedExt) {
+        entry_list entries;
+        for (const auto & entry : std_fs::directory_iterator(path)) {
+            const auto & entryPath = std_fs::relative(entry.path());
+            if (entry.is_directory()) {
+                entries.emplace_back(
+                    std::make_shared<Entry>(entryPath.string(), std::move(readdirRecEntries(entryPath)))
+                );
+            } else if (entry.is_regular_file()) {
+                if (entryPath.extension() != allowedExt) {
+                    continue;
+                }
+
+                entries.emplace_back(readfile(entryPath));
+            }
+        }
+        return std::move(entries);
+    }
+
+    entry_ptr readDirRec(const std_fs::path & path, const std::string & allowedExt) {
+        return std::make_shared<Entry>(path, readdirRecEntries(path, allowedExt));
+    }
+}
