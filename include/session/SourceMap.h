6cd3d31018e8363aea7c3dbc51dcf8613e28b1af
is_gordeev <gordeev.john@gmail.com>
2021-05-29 15:58:18 +0300

    Update `SourceMap` structure

--- include/session/SourceMap.h
+++ include/session/SourceMap.h
@@ -7,24 +7,20 @@
 
 #include "utils/map.h"
 #include "utils/hash.h"
+#include "utils/fs.h"
 #include "data_types/Option.h"
 #include "span/Span.h"
 
 namespace jc::sess {
-    using source_lines = std::vector<std::string>;
+    using source_t = utils::fs::entry_ptr;
     using file_id_t = size_t;
-
-    struct Source {
-        dt::Option<source_lines> sourceLines;
-        std::string path;
-    };
 
     struct SourceMap {
         SourceMap() = default;
 
         file_id_t addSource(const std::string & path);
-        void setSourceLines(file_id_t fileId, source_lines && sourceLines);
-        const Source & getSource(file_id_t) const;
+        void setSourceLines(file_id_t fileId, source_t && source);
+        const source_t & getSource(file_id_t) const;
         size_t getLinesCount(file_id_t) const;
 
         std::string getLine(file_id_t fileId, size_t index) const;
@@ -33,7 +29,7 @@ namespace jc::sess {
         std::string sliceBySpan(file_id_t, const span::Span & span);
 
     private:
-        std::map<file_id_t, Source> sources;
+        std::map<file_id_t, source_t> sources;
     };
 }
 
