cc3a7da0bd5c824a689e5963c59bbd40964089b8
is_gordeev <is_gordeev@bingo-boom.com>
2021-06-03 20:09:57 +0300

    save WIP

---
+++ include/ast/expr/StructLiteral.h
@@ -0,0 +1,12 @@
+#ifndef AST_EXPR_STRUCTLITERAL_H
+#define AST_EXPR_STRUCTLITERAL_H
+
+#include "ast/expr/Expr.h"
+
+namespace jc::ast {
+    struct StructLiteral : Expr {
+        
+    }
+}
+
+#endif // AST_EXPR_STRUCTLITERAL_H
