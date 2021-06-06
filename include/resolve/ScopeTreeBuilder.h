db6fc39b22b57c2b1a76dae341d6d48078282255
is_gordeev <gordeev.john@gmail.com>
2021-06-06 11:03:11 +0300

    `ScopeTreeBuilder` establish structure

--- include/resolve/ScopeTreeBuilder.h
+++ include/resolve/ScopeTreeBuilder.h
@@ -4,9 +4,48 @@
 #include "ast/StubVisitor.h"
 
 namespace jc::resolve {
+    struct Scope;
+    using ast::node_id;
+    using ns_map = std::map<std::string, node_id>;
+    using scope_ptr = std::unique_ptr<Scope>;
+
+    enum class Namespace {
+        Value,
+        Type,
+        Lifetime,
+    };
+
+    struct Scope {
+        Scope(scope_ptr && parent) : parent(std::move(parent)) {}
+
+        dt::Option<scope_ptr> parent;
+        ns_map valueNS;
+        ns_map typeNS;
+        ns_map lifetimeNS;
+
+        ns_map & getNS(Namespace ns) {
+            switch (ns) {
+                case Namespace::Value: return valueNS;
+                case Namespace::Type: return typeNS;
+                case Namespace::Lifetime: return lifetimeNS;
+            }
+        }
+    };
+
     class ScopeTreeBuilder : public ast::StubVisitor {
     public:
-        ScopeTreeBuilder() : StubVisitor() {}
+        ScopeTreeBuilder() : StubVisitor("ScopeTreeBuilder") {}
+
+        void visit(const ast::Mod & mod) override;
+
+    private:
+        common::Logger log{"ScopeTreeBuilder"};
+
+        scope_ptr scope;
+        void declare(Namespace ns, const std::string & name, node_id nodeId);
+
+        void enterScope();
+        void exitScope();
     };
 }
 
--- src/resolve/ScopeTreeBuilder.cpp
+++ src/resolve/ScopeTreeBuilder.cpp
@@ -1,5 +1,26 @@
 #include "resolve/ScopeTreeBuilder.h"
 
 namespace jc::resolve {
+    void ScopeTreeBuilder::visit(const ast::Mod & mod) {
+        enterScope();
+        exitScope();
+    }
 
+    void ScopeTreeBuilder::declare(Namespace ns, const std::string & name, node_id nodeId) {
+        auto & map = scope->getNS(ns);
+        if (utils::map::has(map, name)) {
+            // TODO: Suggestions
+            log.error(name + "has been already declared in this scope");
+            return;
+        }
+        map[name] = nodeId;
+    }
+
+    void ScopeTreeBuilder::enterScope() {
+        scope = std::make_unique<Scope>(std::move(scope));
+    }
+
+    void ScopeTreeBuilder::exitScope() {
+        scope = std::move(scope->parent.unwrap("[ScopeTreeBuilder]: Tried to exit global scope"));
+    }
 }
