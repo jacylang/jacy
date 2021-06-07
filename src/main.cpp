458bec80ae78d2cd93cbd722a2857c29863fd19d
hazer-hazer <gordeev.john@gmail.com>
2021-06-08 00:16:59 +0300

    Attempt to implement stacktrace printing 😢

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "core/Jacy.h"
+#include "platform/signals.h"
 
 int main(int argc, const char ** argv) {
+    setSignals(argc, argv);
     const auto jacy = new jc::core::Jacy();
     try {
         jacy->meow(argc, argv);
