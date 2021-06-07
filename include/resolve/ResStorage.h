558b5c30e8b2b1fa03c51451d55fbac874d808d4
is_gordeev <gordeev.john@gmail.com>
2021-06-08 00:44:29 +0300

    Update `ResStorage`

--- include/resolve/ResStorage.h
+++ include/resolve/ResStorage.h
@@ -8,12 +8,18 @@
 namespace jc::resolve {
     using ast::node_id;
 
+    /// ResStorage
+    /// @brief Collection of {path: node} names resolutions
     class ResStorage {
     public:
         ResStorage() = default;
 
         node_id getRes(node_id name) const {
             return resolutions.at(name);
+        }
+
+        void setRes(node_id name, node_id res) {
+            resolutions.emplace(name, res);
         }
 
     private:
