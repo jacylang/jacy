d4549aec1a6ae61b80fcbb423252ae6b5518dd70
is_gordeev <gordeev.john@gmail.com>
2021-06-08 23:33:39 +0300

    Fix `Infix` node warnings

--- include/ast/expr/Infix.h
+++ include/ast/expr/Infix.h
@@ -9,7 +9,7 @@ namespace jc::ast {
 
     struct Infix : Expr {
         Infix(expr_ptr lhs, const parser::Token & op, expr_ptr rhs, const Span & span)
-            : lhs(std::move(lhs)), op(op), rhs(std::move(rhs)), Expr(span, ExprKind::Infix) {}
+            : Expr(span, ExprKind::Infix), lhs(std::move(lhs)), op(op), rhs(std::move(rhs)) {}
 
         expr_ptr lhs;
         parser::Token op;
