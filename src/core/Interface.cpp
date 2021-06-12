fd7be486461bba50efd4f62b435b5f87650d7164
is_gordeev <gordeev.john@gmail.com>
2021-06-12 20:48:36 +0300

    Unwrap `Linter` suggestions

--- src/core/Interface.cpp
+++ src/core/Interface.cpp
@@ -65,7 +65,7 @@ namespace jc::core {
     void Interface::lintAst() {
         log.printTitleDev("Linting AST");
 
-        linter.lint(*party.unwrap());
+        linter.lint(*party.unwrap()).unwrap(sess);
     }
 
     ast::dir_module_ptr Interface::parseDir(const fs::entry_ptr & dir, const std::string & ignore) {
