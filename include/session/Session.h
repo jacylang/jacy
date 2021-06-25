1ab2db89b3ecab348a7994fc27e0dd6e442fe997
is_gordeev <gordeev.john@gmail.com>
2021-06-26 01:23:34 +0300

    Fix `sess::Session`

--- include/session/Session.h
+++ include/session/Session.h
@@ -19,7 +19,7 @@ namespace jc::sess {
     struct Session {
         SourceMap sourceMap;
         ast::NodeMap nodeMap;
-        dt::Option<resolve::module_ptr> modTreeRoot;
+        dt::Option<resolve::module_ptr> modTreeRoot{dt::None};
         resolve::DefStorage defStorage;
         resolve::ResStorage resStorage;
     };
