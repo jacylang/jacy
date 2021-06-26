8212961ca0b3b483994c549c06b9c7c140fa488b
is_gordeev <gordeev.john@gmail.com>
2021-06-26 18:01:10 +0300

    Update `ModuleTreeBuilder::enterMod` remove double definition

--- src/resolve/ModuleTreeBuilder.cpp
+++ src/resolve/ModuleTreeBuilder.cpp
@@ -137,13 +137,12 @@ namespace jc::resolve {
     void ModuleTreeBuilder::enterMod(const std::string & name, const dt::Option<ast::Span> & nameSpan, def_id defId) {
         log.dev("Enter mod '", name, "'");
         auto child = std::make_shared<Module>(defId, mod);
-        if (utils::map::has(mod->typeNS, name)) {
+        if (utils::map::has(mod->children, name)) {
             if (not nameSpan) {
                 log.devPanic("Module without name span redeclaration");
             }
             suggestErrorMsg("'" + name + "' has been already declared in this scope", nameSpan.unwrap());
         } else {
-            mod->typeNS.emplace(name, defId);
             mod->children.emplace(name, child);
         }
 
