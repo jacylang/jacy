87b0b1fa3677c2dd3b50dfac51e50d724d1a177f
is_gordeev <gordeev.john@gmail.com>
2021-07-01 00:06:27 +0300

    Update `ast::TupleExpr`, make elements unnamed

--- include/ast/expr/TupleExpr.h
+++ include/ast/expr/TupleExpr.h
@@ -1,14 +1,14 @@
 #ifndef JACY_AST_EXPR_TUPLEEXPR_H
 #define JACY_AST_EXPR_TUPLEEXPR_H
 
-#include "ast/fragments/Arg.h"
+#include "ast/expr/Expr.h"
 
 namespace jc::ast {
     struct TupleExpr : Expr {
-        TupleExpr(arg_list elements, const Span & span)
+        TupleExpr(expr_list elements, const Span & span)
             : Expr(span, ExprKind::Tuple), elements(std::move(elements)) {}
 
-        arg_list elements;
+        expr_list elements;
 
         void accept(BaseVisitor & visitor) const override {
             return visitor.visit(*this);
