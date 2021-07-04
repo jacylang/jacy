0c398b0dbbafce5979419fdc887500e48236c080
is_gordeev <gordeev.john@gmail.com>
2021-07-04 19:47:37 +0300

    Update `pat_ptr`

--- include/ast/fragments/Pattern.h
+++ include/ast/fragments/Pattern.h
@@ -8,7 +8,7 @@
 namespace jc::ast {
     struct Pattern;
     struct BorrowPat;
-    using pat_ptr = std::shared_ptr<Pattern>;
+    using pat_ptr = PR<std::shared_ptr<Pattern>>;
     using opt_pat = dt::Option<pat_ptr>;
     using pat_list = std::vector<pat_ptr>;
     using id_pat_ptr = std::shared_ptr<BorrowPat>;
