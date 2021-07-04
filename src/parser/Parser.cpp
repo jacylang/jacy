bd9afac5d452faa9faf993abff144a26a2a5012e
is_gordeev <gordeev.john@gmail.com>
2021-07-04 22:57:41 +0300

    Update `Parser` `Pattern` pointers

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -2338,7 +2338,7 @@ namespace jc::parser {
         auto token = peek();
         advance();
 
-        return makeNode<LitPat>(neg, token, begin.to(cspan()));
+        return makeNode<LitPat>(neg, token, begin.to(cspan())).as<Pattern>();
     }
 
     pure_pat_ptr Parser::parseBorrowPat() {
@@ -2355,7 +2355,7 @@ namespace jc::parser {
             pat = parsePat();
         }
 
-        return makeNode<BorrowPat>(ref, mut, std::move(id), std::move(pat), begin.to(id.span()));
+        return makeNode<BorrowPat>(ref, mut, std::move(id), std::move(pat), begin.to(id.span())).as<Pattern>();
     }
 
     pure_pat_ptr Parser::parseRefPat() {
@@ -2366,7 +2366,7 @@ namespace jc::parser {
         bool mut = skipOpt(TokenKind::Mut);
         auto pat = parsePat();
 
-        return makeNode<RefPat>(ref, mut, std::move(pat), begin.to(cspan()));
+        return makeNode<RefPat>(ref, mut, std::move(pat), begin.to(cspan())).as<Pattern>();
     }
 
     pure_pat_ptr Parser::parseStructPat(path_expr_ptr && path) {
@@ -2427,7 +2427,7 @@ namespace jc::parser {
 
         skip(TokenKind::RBrace, "Missing closing `}` in struct pattern", Recovery::None);
 
-        return makeNode<StructPat>(std::move(path), std::move(elements), begin.to(cspan()));
+        return makeNode<StructPat>(std::move(path), std::move(elements), begin.to(cspan())).as<Pattern>();
     }
 
     // Helpers //
