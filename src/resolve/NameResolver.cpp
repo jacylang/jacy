945600aa2d0d298d284c356531666b0fee00a29b
is_gordeev <gordeev.john@gmail.com>
2021-07-09 01:16:17 +0300

    Update Agenda

--- src/resolve/NameResolver.cpp
+++ src/resolve/NameResolver.cpp
@@ -353,8 +353,11 @@ namespace jc::resolve {
             }
             suggestErrorMsg(msg, unresolvedSegIdent->span);
 
-            // Agenda: Fix alt names, something wrong with them.
-            //  Due to something, [FILE] provided as suggestion, checkout current commit example to test
+            // Agenda:
+            //  - Fix alt names, something wrong with them.
+            //    Due to something, [FILE] provided as suggestion, checkout current commit example to test
+            //  - What to do next?
+            //    - Visibility
             suggestAltNames(ns, unresolvedSegName, altDefs);
         }
     }
