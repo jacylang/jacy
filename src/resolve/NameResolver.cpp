75b9245c3db4d8124a6e324ea0b9600127e9e28d
is_gordeev <gordeev.john@gmail.com>
2021-07-10 01:18:20 +0300

    Update `NameResolver::resolvePath`, start search from module but not a definition

--- src/resolve/NameResolver.cpp
+++ src/resolve/NameResolver.cpp
@@ -313,15 +313,17 @@ namespace jc::resolve {
             if (i < path.segments.size() - 1) {
                 // Note: Module-like items stored in `Type` namespace
                 searchMod->find(Namespace::Type, segName).then([&](def_id defId) {
+                    const auto & descendModule = sess->defStorage.getModule(defId);
+
                     // Check module definition visibility
                     // Note: Order matters, we need to check for visibility before descend to next module
-                    if (sess->defStorage.isPrivateFor(defId, searchMod->nearestModDef)) {
+                    if (sess->defStorage.isPrivateFor(descendModule->defId, searchMod->nearestModDef)) {
                         inaccessible = true;
                         unresolvedSegIndex = i;
                     }
 
                     // Get module specified in path segment from current searched module
-                    searchMod = sess->defStorage.getModule(defId);
+                    searchMod = descendModule;
 
                     log.dev("Enter module by path segment '", pathStr, "' with def id #", defId);
                 }).otherwise([&]() {
