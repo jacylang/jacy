bd258c22df0c51866fbc6dde3182eef414b155e8
is_gordeev <gordeev.john@gmail.com>
2021-07-09 23:54:23 +0300

    Update `Definition`

--- include/resolve/Definition.h
+++ include/resolve/Definition.h
@@ -10,6 +10,7 @@ namespace jc::resolve {
     using opt_module_ptr = dt::Option<module_ptr>;
     using def_id = size_t;
     using opt_def_id = dt::Option<def_id>;
+    using mod_depth = uint32_t;
 
     enum class Namespace {
         Value,
@@ -216,59 +217,6 @@ namespace jc::resolve {
             }
             return os;
         }
-    };
-
-    struct DefStorage {
-        const Def & getDef(def_id defId) const {
-            try {
-                return defs.at(defId);
-            } catch (std::out_of_range & e) {
-                common::Logger::devPanic("Called `DefStorage::getDef` with non-existent `defId`");
-            }
-        }
-
-        DefVis isPrivateFor(def_id defId) const {
-            return getDef(defId).vis;
-        }
-
-        template<class ...Args>
-        def_id define(Args ...args) {
-            defs.emplace_back(Def(args...));
-            return defs.size() - 1;
-        }
-
-        const std::vector<Def> & getDefinitions() const {
-            return defs;
-        }
-
-        module_ptr addModule(def_id defId, module_ptr module) {
-            const auto & added = modules.emplace(defId, module);
-            if (not added.second) {
-                common::Logger::devPanic("[DefStorage]: Tried to add module with same defId twice");
-            }
-            return added.first->second;
-        }
-
-        module_ptr addBlock(ast::node_id nodeId, module_ptr module) {
-            const auto & added = blocks.emplace(nodeId, module);
-            if (not added.second) {
-                common::Logger::devPanic("[DefStorage]: Tried to add block with same nodeId twice");
-            }
-            return added.first->second;
-        }
-
-        const module_ptr & getModule(def_id defId) const {
-            return modules.at(defId);
-        }
-
-        const module_ptr & getBlock(ast::node_id nodeId) const {
-            return blocks.at(nodeId);
-        }
-
-    private:
-        std::vector<Def> defs;
-        std::map<def_id, module_ptr> modules;
-        std::map<ast::node_id, module_ptr> blocks;
     };
 }
 
