ce38925703cd32ee15c55ef6b320d54e6dcfd9c6
is_gordeev <gordeev.john@gmail.com>
2021-07-11 12:50:10 +0300

    Update `Importer` `UseTreeRaw`

--- src/resolve/Importer.cpp
+++ src/resolve/Importer.cpp
@@ -23,23 +23,22 @@ namespace jc::resolve {
         // TODO!!!: Unify path resolution logic in NameResolver and Importer. It might be impossible btw
         // TODO!!!: `pub use...` reexports, now all `use`s are public
 
-        resolvePath(PathResKind::Prefix, *useTree.path, [&](const DefPerNS & defPerNs, const ast::SimplePathSeg & seg) {
-            defPerNs.each([&](const opt_def_id & optDefId, Namespace nsKind) {
-                optDefId.then([&](def_id defId) {
-                    const auto & segName = seg.ident.unwrap().unwrap()->getValue();
-                    _useDeclModule->tryDefine(nsKind, segName, defId).then([&](def_id oldDefId) {
-                        // Note: If some definition can be redefined -- it is always named definition,
-                        //  so we can safely get its name node span
-                        const auto & oldDef = sess->defStorage.getDef(oldDefId);
-                        const auto & oldDefSpan = sess->nodeMap.getNodeSpan(oldDef.nameNodeId.unwrap());
-                        suggest(
-                            std::make_unique<sugg::MsgSpanLinkSugg>(
-                                "Cannot `use` '" + segName + "'",
-                                seg.span,
-                                "Because it is already declared as " + oldDef.kindStr() + " here",
-                                oldDefSpan,
-                                sugg::SuggKind::Error));
-                    });
+        const auto & pathResult = resolvePath(PathResKind::Prefix, *useTree.path);
+        pathResult.defPerNs.each([&](const opt_def_id & optDefId, Namespace nsKind) {
+            optDefId.then([&](def_id defId) {
+                const auto & segName = pathResult.lastSeg->ident.unwrap().unwrap()->getValue();
+                _useDeclModule->tryDefine(nsKind, segName, defId).then([&](def_id oldDefId) {
+                    // Note: If some definition can be redefined -- it is always named definition,
+                    //  so we can safely get its name node span
+                    const auto & oldDef = sess->defStorage.getDef(oldDefId);
+                    const auto & oldDefSpan = sess->nodeMap.getNodeSpan(oldDef.nameNodeId.unwrap());
+                    suggest(
+                        std::make_unique<sugg::MsgSpanLinkSugg>(
+                            "Cannot `use` '" + segName + "'",
+                            seg.span,
+                            "Because it is already declared as " + oldDef.kindStr() + " here",
+                            oldDefSpan,
+                            sugg::SuggKind::Error));
                 });
             });
         });
