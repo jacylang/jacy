2a96fc91b014c4e378736c3ad7bf92ee5dfa05cf
is_gordeev <gordeev.john@gmail.com>
2021-07-12 09:41:29 +0300

    Update `NParseResult`

--- include/ast/Node.h
+++ include/ast/Node.h
@@ -49,83 +49,12 @@ namespace jc::ast {
     /// Base class for ParseResult (for non-boxed nodes) and NParseResult (for boxed nodes)
     /// Defines common methods
 
-    template<class T>
-    class NParseResult {
-    public:
-        using E = N<ErrorNode>;
-        using S = std::variant<T, E, std::monostate>;
+    template<class U>
+    class NParseResult : public dt::Result<U, ErrorNode> {
+        using E = typename dt::Result<U, ErrorNode>::error_type;
+        using T = typename dt::Result<U, ErrorNode>::value_type;
 
     public:
-        NParseResult() : state(std::monostate{}) {}
-        NParseResult(T && value) : state(std::move(value)) {}
-        NParseResult(E && error) : state(std::move(error)) {}
-        NParseResult(NParseResult<T> && other)
-            : state(std::move(other.state)) {}
-
-        NParseResult<T> & operator=(const NParseResult<T> & other) {
-            if (other.err()) {
-                this->state = std::get<E>(other.state).release();
-            } else {
-                this->state = std::get<T>(other.state).release();
-            }
-            return *this;
-        }
-
-        NParseResult<T> & operator=(NParseResult<T> && other) {
-            if (other.err()) {
-                this->state = std::get<E>(std::move(other.state));
-            } else {
-                this->state = std::get<T>(std::move(other.state));
-            }
-            return *this;
-        }
-
-        NParseResult<T> & operator=(T && rawT) {
-            this->state = std::move(rawT);
-            return *this;
-        }
-
-        NParseResult<T> & operator=(E && rawE) {
-            this->state = std::move(rawE);
-            return *this;
-        }
-
-        bool ok() const {
-            return not err();
-        }
-
-        bool err() const {
-            return state.index() != 0;
-        }
-
-        T take(const std::string & msg = "") {
-            if (this->err()) {
-                throw std::logic_error(msg.empty() ? "Called `ParseResult::take` on an `Err` ParseResult" : msg);
-            }
-            return std::get<T>(std::move(state));
-        }
-
-        const T & unwrap(const std::string & msg = "") const {
-            if (this->err()) {
-                throw std::logic_error(msg.empty() ? "Called `ParseResult::unwrap` on an `Err` ParseResult" : msg);
-            }
-            return std::get<T>(state);
-        }
-
-        const E & asErr() const {
-            if (this->ok()) {
-                throw std::logic_error("Called `ParseResult::asErr` on an non-error ParseResult");
-            }
-            return std::get<E>(state);
-        }
-
-        const T & asValue() const {
-            if (this->err()) {
-                throw std::logic_error("Called `ParseResult::asValue` on an `Err` ParseResult");
-            }
-            return std::get<T>(state);
-        }
-        
         template<class B>
         NParseResult<N<B>> as() {
             if (this->err()) {
@@ -148,116 +77,7 @@ namespace jc::ast {
             }
             return std::get<T>(this->state)->span;
         }
-
-    protected:
-        S state;
     };
-
-//    template<class T>
-//    class ParseResult {
-//    public:
-//        using E = ErrorNode;
-//        using S = std::variant<T, E, std::monostate>;
-//
-//    public:
-//        ParseResult() : state(std::monostate{}) {}
-//        ParseResult(T && value) : state(std::move(value)) {}
-//        ParseResult(E && error) : state(std::move(error)) {}
-//        ParseResult(const ParseResult<T> & other)
-//            : state(std::move(other.state)) {}
-//        ParseResult(ParseResult<T> && other)
-//            : state(std::move(other.state)) {}
-//
-//        ParseResult<T> & operator=(const ParseResult<T> & other) {
-//            if (other.err()) {
-//                this->state = std::get<E>(other.state).release();
-//            } else {
-//                this->state = std::get<T>(other.state).relear();
-//            }
-//            return *this;
-//        }
-//
-//        ParseResult<T> & operator=(ParseResult<T> && other) {
-//            if (other.err()) {
-//                this->state = std::get<E>(std::move(other.state));
-//            } else {
-//                this->state = std::get<T>(std::move(other.state));
-//            }
-//            return *this;
-//        }
-//
-//        ParseResult<T> & operator=(T && rawT) {
-//            this->state = std::move(rawT);
-//            return *this;
-//        }
-//
-//        ParseResult<T> & operator=(E && rawE) {
-//            this->state = std::move(rawE);
-//            return *this;
-//        }
-//
-//        bool ok() const {
-//            return not err();
-//        }
-//
-//        bool err() const {
-//            return state.index() != 0;
-//        }
-//
-//        T take(const std::string & msg = "") {
-//            if (this->err()) {
-//                throw std::logic_error(msg.empty() ? "Called `ParseResult::take` on an `Err` ParseResult" : msg);
-//            }
-//            return std::get<T>(std::move(state));
-//        }
-//
-//        const T & unwrap(const std::string & msg = "") const {
-//            if (this->err()) {
-//                throw std::logic_error(msg.empty() ? "Called `ParseResult::unwrap` on an `Err` ParseResult" : msg);
-//            }
-//            return std::get<T>(state);
-//        }
-//
-//        const E & asErr() const {
-//            if (this->ok()) {
-//                throw std::logic_error("Called `ParseResult::asErr` on an non-error ParseResult");
-//            }
-//            return std::get<E>(state);
-//        }
-//
-//        const T & asValue() const {
-//            if (this->err()) {
-//                throw std::logic_error("Called `ParseResult::asValue` on an `Err` ParseResult");
-//            }
-//            return std::get<T>(state);
-//        }
-//
-//        template<class B>
-//        ParseResult<N<B>> as() {
-//            if (this->err()) {
-//                return ParseResult<N<B>>(std::move(std::get<E>(this->state)));
-//            }
-//            return ParseResult<N<B>>(N<B>(static_cast<B*>(std::get<T>(this->state).release())));
-//        }
-//
-//        void autoAccept(BaseVisitor & visitor) const {
-//            if (this->err()) {
-//                return std::get<E>(this->state).accept(visitor);
-//            } else {
-//                return std::get<T>(this->state).accept(visitor);
-//            }
-//        }
-//
-//        const Span & span() const {
-//            if (this->err()) {
-//                return std::get<E>(this->state).span;
-//            }
-//            return std::get<T>(this->state).span;
-//        }
-//
-//    protected:
-//        S state;
-//    };
 
     template<class T>
     inline NParseResult<N<T>> OkPR(N<T> && ok) {
