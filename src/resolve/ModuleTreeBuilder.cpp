77e2f01f098aeb1112fe479626caa60c6a20950e
is_gordeev <gordeev.john@gmail.com>
2021-07-14 20:11:05 +0300

    Update `ModuleTreeBuilder::suggestCannotRedefine`

--- src/resolve/ModuleTreeBuilder.cpp
+++ src/resolve/ModuleTreeBuilder.cpp
@@ -249,7 +249,7 @@ namespace jc::resolve {
         def_id prevDefId
     ) {
         const auto & prevDef = _defStorage.getDef(prevDefId);
-        const auto & prevDefSpan = sess->nodeMap.getNodeSpan(prevDef.nameNodeId.unwrap());
+        const auto & prevDefSpan = sess->nodeSpanMap.at(prevDef.nameNodeId.unwrap());
         suggest(
             std::make_unique<sugg::MsgSpanLinkSugg>(
                 "Cannot redeclare '" + ident.unwrap()->getValue() + "' as " + Def::kindStr(as),
