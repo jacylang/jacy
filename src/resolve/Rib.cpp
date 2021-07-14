42f5e9a9ea65f051019a03a34ef67b329e51d743
is_gordeev <gordeev.john@gmail.com>
2021-07-14 20:53:49 +0300

    Update `Rib` `Ident` usages

--- src/resolve/Rib.cpp
+++ src/resolve/Rib.cpp
@@ -2,10 +2,10 @@
 
 namespace jc::resolve {
     opt_node_id Rib::define(const ast::ident_pr & ident) {
-        const auto & name = ident.unwrap()->getValue();
+        const auto & name = ident.unwrap().getValue();
         const auto & found = locals.find(name);
         if (found == locals.end()) {
-            locals.emplace(name, ident.unwrap()->id);
+            locals.emplace(name, ident.unwrap().id);
             return None;
         }
         return found->second;
