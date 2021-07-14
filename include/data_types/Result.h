7bae14a8f2961b89861d737e10f1cd032994f6ea
is_gordeev <gordeev.john@gmail.com>
2021-07-14 06:57:47 +0300

    Update `Result`

--- include/data_types/Result.h
+++ include/data_types/Result.h
@@ -281,12 +281,7 @@ namespace jc::dt {
                       "Cannot create a Result<T, E> object with E=void. You want an "
                       "optional<T>.");
 
-//        constexpr Result() {
-//            static_assert(std::is_default_constructible<T>::value,
-//                          "Result<T, E> may only be default constructed if T is default "
-//                          "constructable.");
-//            m_storage = Ok(T());
-//        }
+        constexpr Result() noexcept(std::is_default_constructible<T>::value) : m_storage(Ok(T())) {}
 
 //        constexpr Result(Ok<T> value) : m_storage(std::move(value)) {}
 //        constexpr Result(Err<E> value) : m_storage(std::move(value)) {}
