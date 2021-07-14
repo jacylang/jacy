b012e7a9788c0a6a64895f10ab5ae56c60d0faa2
is_gordeev <gordeev.john@gmail.com>
2021-07-14 04:32:41 +0300

    Update `Parser::primary`

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1149,7 +1149,7 @@ namespace jc::parser {
             auto pathExpr = parsePathExpr();
             if (is(TokenKind::LBrace)) {
                 if (pathExpr.err()) {
-                    return parseStructExpr(makeErrNode(pathExpr.span()));
+                    return parseStructExpr(makeErrPR<N<PathExpr>>(pathExpr.span()));
                 }
                 return parseStructExpr(pathExpr.take());
             }
