af10561b40dbb8d25f2f751c8a3548cd62c7b7dc
is_gordeev <gordeev.john@gmail.com>
2021-07-16 22:24:55 +0300

    Update `Arg`

--- include/ast/fragments/Arg.h
+++ include/ast/fragments/Arg.h
@@ -9,7 +9,7 @@ namespace jc::ast {
     using arg_list = std::vector<Arg>;
 
     struct Arg : Node {
-        Arg(opt_ident name, expr_ptr value, const Span & span)
+        Arg(opt_ident && name, expr_ptr && value, const Span & span)
             : Node(span), name(std::move(name)), value(std::move(value)) {}
 
         opt_ident name;
