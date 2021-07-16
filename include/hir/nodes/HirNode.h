dd6cb0a8d244bc1391b37f4f134122119e80085e
is_gordeev <gordeev.john@gmail.com>
2021-07-17 00:32:14 +0300

    Add `HirId`

--- include/hir/nodes/HirNode.h
+++ include/hir/nodes/HirNode.h
@@ -2,9 +2,14 @@
 #define JACY_HIR_NODES_HIRNODE_H
 
 #include "span/Span.h"
+#include "resolve/Definition.h"
 
 namespace jc::hir {
     using span::Span;
+
+    struct HirId {
+        resolve::DefId defId;
+    };
 
     struct HirNode {
         HirNode(const Span & span) : span(span) {}
