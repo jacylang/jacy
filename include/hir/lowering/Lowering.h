7c3d0f45d0e31348d86d1892c83f780884ce37c5
is_gordeev <gordeev.john@gmail.com>
2021-07-17 18:48:06 +0300

    Update `Lowering`

--- include/hir/lowering/Lowering.h
+++ include/hir/lowering/Lowering.h
@@ -1,11 +1,19 @@
 #ifndef JACY_HIR_LOWERING_H
 #define JACY_HIR_LOWERING_H
 
-#include "ast/BaseVisitor.h"
+#include "ast/nodes.h"
+#include "session/Session.h"
 
 namespace jc::hir {
     class Lowering {
+    public:
+        Lowering() = default;
+        virtual ~Lowering() = default;
 
+        void lower(const sess::sess_ptr & sess, const ast::Party & party);
+
+    private:
+        
     };
 }
 
