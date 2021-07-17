46d886235871690fa3fa53c35cd53755db248f58
is_gordeev <gordeev.john@gmail.com>
2021-07-17 15:17:13 +0300

    Add `span::Ident` constructor from `Token`

--- include/span/Ident.h
+++ include/span/Ident.h
@@ -2,9 +2,17 @@
 #define JACY_SPAN_IDENT_H
 
 #include "span/Span.h"
+#include "parser/Token.h"
 
 namespace jc::span {
     struct Ident {
+        Ident(const parser::Token & token) {
+            if (token.kind != parser::TokenKind::Id) {
+                throw std::logic_error("Unexpected kind of token for `Ident` constructor");
+            }
+            name = token.val;
+            span = token.span;
+        }
         Ident(const std::string & name, const Span & span) : name(name), span(span) {}
 
         std::string name;
