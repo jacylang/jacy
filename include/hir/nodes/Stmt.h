cce5c3610b2eac2780c2ca6fbb1114aa13fe9976
is_gordeev <gordeev.john@gmail.com>
2021-07-18 00:40:52 +0300

    Add `ExprStmt`

--- include/hir/nodes/Stmt.h
+++ include/hir/nodes/Stmt.h
@@ -2,6 +2,7 @@
 #define JACY_HIR_NODES_STMT_H
 
 #include "hir/nodes/HirNode.h"
+#include "hir/nodes/fragments.h"
 
 namespace jc::hir {
     enum class StmtKind {
@@ -14,6 +15,13 @@ namespace jc::hir {
         Stmt(StmtKind kind, const HirId & hirId, const Span & span) : HirNode(hirId, span), kind(kind) {}
 
         StmtKind kind;
+    };
+
+    struct ExprStmt : Stmt {
+        ExprStmt(expr_ptr && expr, const HirId & hirId, const Span & span)
+            : Stmt(StmtKind::Expr, hirId, span), expr(std::move(expr)) {}
+
+        expr_ptr expr;
     };
 }
 
