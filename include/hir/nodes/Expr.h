850d9a4f001554b10ed417814540b02fac195050
is_gordeev <gordeev.john@gmail.com>
2021-07-17 17:58:19 +0300

    Put all `hir` expressions to nested `expr` namespace

--- include/hir/nodes/Expr.h
+++ include/hir/nodes/Expr.h
@@ -5,16 +5,10 @@
 #include "hir/nodes/fragments.h"
 
 namespace jc::hir {
-    // It would be nice to have Expr and all other nodes as value types,
-    // anyway, it would require usage of std::variant which is really inconvenient to work with when there're
-    // so many types. Thus I'll just box Expr.
-    struct Expr;
-    using expr_ptr = N<Expr>;
-    using expr_list = std::vector<expr_ptr>;
-
     enum class ExprKind {
         Array,
         Assign,
+        Block,
         If,
         Infix,
         Invoke,
@@ -27,56 +21,65 @@ namespace jc::hir {
         Tuple,
     };
 
-    struct Expr : HirNode {
-        Expr(ExprKind kind, const HirId & hirId, const Span & span) : HirNode(hirId, span), kind(kind) {}
+    namespace expr {
+        // It would be nice to have Expr and all other nodes as value types,
+        // anyway, it would require usage of std::variant which is really inconvenient to work with when there're
+        // so many types. Thus I'll just box Expr.
+        struct Expr;
+        using expr_ptr = N<Expr>;
+        using expr_list = std::vector<expr_ptr>;
 
-        ExprKind kind;
-    };
+        struct Expr : HirNode {
+            Expr(ExprKind kind, const HirId & hirId, const Span & span) : HirNode(hirId, span), kind(kind) {}
 
-    struct Array : Expr {
-        Array(expr_list && elements, const HirId & hirId, const Span & span)
-            : Expr(ExprKind::Array, hirId, span), elements(std::move(elements)) {}
+            ExprKind kind;
+        };
 
-        expr_list elements;
-    };
+        struct Array : Expr {
+            Array(expr_list && elements, const HirId & hirId, const Span & span)
+                : Expr(ExprKind::Array, hirId, span), elements(std::move(elements)) {}
 
-    struct Assign : Expr {
-        Assign(expr_ptr && lhs, expr_ptr && rhs, const HirId & hirId, const Span & span)
-            : Expr(ExprKind::Assign, hirId, span), lhs(std::move(lhs)), rhs(std::move(rhs)) {}
+            expr_list elements;
+        };
 
-        expr_ptr lhs;
-        expr_ptr rhs;
-    };
+        struct Assign : Expr {
+            Assign(expr_ptr && lhs, expr_ptr && rhs, const HirId & hirId, const Span & span)
+                : Expr(ExprKind::Assign, hirId, span), lhs(std::move(lhs)), rhs(std::move(rhs)) {}
 
-    struct Invoke : Expr {
-        Invoke(expr_ptr && lhs, arg_list && args, const HirId & hirId, const Span & span)
-            : Expr(ExprKind::Invoke, hirId, span), lhs(std::move(lhs)), args(std::move(args)) {}
+            expr_ptr lhs;
+            expr_ptr rhs;
+        };
 
-        expr_ptr lhs;
-        arg_list args;
-    };
+        struct Invoke : Expr {
+            Invoke(expr_ptr && lhs, arg_list && args, const HirId & hirId, const Span & span)
+                : Expr(ExprKind::Invoke, hirId, span), lhs(std::move(lhs)), args(std::move(args)) {}
 
-    struct Literal : Expr {
-        // FIXME: Unify `Literal` usage for AST and HIR
-        Literal(const parser::Token & token, const HirId & hirId, const Span & span)
-            : Expr(ExprKind::Literal, hirId, span), token(token) {}
+            expr_ptr lhs;
+            arg_list args;
+        };
 
-        parser::Token token;
-    };
+        struct Literal : Expr {
+            // FIXME: Unify `Literal` usage for AST and HIR
+            Literal(const parser::Token & token, const HirId & hirId, const Span & span)
+                : Expr(ExprKind::Literal, hirId, span), token(token) {}
 
-    struct Return : Expr {
-        Return(expr_ptr && expr, const HirId & hirId, const Span & span)
-            : Expr(ExprKind::Return, hirId, span), expr(std::move(expr)) {}
+            parser::Token token;
+        };
 
-        expr_ptr expr;
-    };
+        struct Return : Expr {
+            Return(expr_ptr && expr, const HirId & hirId, const Span & span)
+                : Expr(ExprKind::Return, hirId, span), expr(std::move(expr)) {}
 
-    struct Tuple : Expr {
-        Tuple(expr_list && values, const HirId & hirId, const Span & span)
-            : Expr(ExprKind::Tuple, hirId, span), values(std::move(values)) {}
+            expr_ptr expr;
+        };
 
-        expr_list values;
-    };
+        struct Tuple : Expr {
+            Tuple(expr_list && values, const HirId & hirId, const Span & span)
+                : Expr(ExprKind::Tuple, hirId, span), values(std::move(values)) {}
+
+            expr_list values;
+        };
+    }
 }
 
 #endif // JACY_HIR_NODES_EXPR_H
