3c0d7e5de0edd8c5f6341bb9cb79fa0cff44fdf7
is_gordeev <gordeev.john@gmail.com>
2021-07-18 18:29:21 +0300

    Update `Interface::parse`

--- src/core/Interface.cpp
+++ src/core/Interface.cpp
@@ -66,7 +66,7 @@ namespace jc::core {
         auto rootFile = parseFile(std::move(rootFileEntry));
 
         auto rootDir = parseDir(
-            fs::readDirRec(rootFileEntry.getPath().parent_path(), ".jc"),
+            fs::readDirRec(rootDirPath, ".jc"),
             rootFileName
         );
 
