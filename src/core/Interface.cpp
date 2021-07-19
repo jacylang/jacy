15f5df4ed22387836398c11e124b8bf148196f7b
is_gordeev <gordeev.john@gmail.com>
2021-07-19 06:07:42 +0300

    Update benchmarks table layout

--- src/core/Interface.cpp
+++ src/core/Interface.cpp
@@ -481,12 +481,12 @@ namespace jc::core {
         // Table
         // | Benchmark name | Processed entity (e.g. AST) | time | speed
         // Wrap it to ~120 chars (idk how logger will print it actually), so the layout is following
-        // | 60 | 20 | 10 | 30 (there can be pretty long entity names) |
+        // | 55 | 20 | 15 | 30 (there can be pretty long entity names) |
         // Note: Choose the shortest names for benchmarks!!!
 
-        constexpr uint8_t BNK_NAME_WRAP_LEN = 60;
+        constexpr uint8_t BNK_NAME_WRAP_LEN = 55;
         constexpr uint8_t ENTITY_NAME_WRAP_LEN = 20;
-        constexpr uint8_t TIME_WRAP_LEN = 10;
+        constexpr uint8_t TIME_WRAP_LEN = 15;
         constexpr uint8_t SPEED_WRAP_LEN = 30;
 
         for (const auto & bnk : benchmarks) {
