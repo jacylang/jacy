73a1991c3c1cd5299bb568a59d486237e09595e4
is_gordeev <gordeev.john@gmail.com>
2021-07-19 07:25:26 +0300

    Update `Interface::printBenchmarks`

--- src/core/Interface.cpp
+++ src/core/Interface.cpp
@@ -513,7 +513,7 @@ namespace jc::core {
             ).nl();
 
             if (bnk.kind == common::Config::BenchmarkKind::Stage) {
-                log.tableRow(
+                log.tableHeaderLine(
                     TC("", BNK_NAME_WRAP_LEN),
                     TC("", ENTITY_NAME_WRAP_LEN),
                     TC("", TIME_WRAP_LEN),
