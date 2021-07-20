9bea5808eae153ce0f13afac430694a8ea2a4261
is_gordeev <gordeev.john@gmail.com>
2021-07-21 01:06:06 +0300

    Update `log::Table::corners`

--- src/log/utils.cpp
+++ src/log/utils.cpp
@@ -1,1 +1,10 @@
 #include "log/utils.h"
+
+// Tables //
+namespace jc::log {
+    template<table_size_t Cols>
+    const std::map<CellKind, std::array<std::string, 3>> Table<Cols>::corners = {
+        {CellKind::Value, {"| ", " | ", " |"}},
+        {CellKind::Line, {"+-", "-+-", "-+"}},
+    };
+}
