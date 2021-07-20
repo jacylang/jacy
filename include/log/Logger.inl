de158d41bc603222f43e1614d652903247fcbca4
is_gordeev <gordeev.john@gmail.com>
2021-07-20 21:59:59 +0300

    Move common data structures `std::ostream` overloads to `log::utils`

--- include/log/Logger.inl
+++ include/log/Logger.inl
@@ -1,46 +1,5 @@
 #include "log/Logger.h"
 
-/**
- * std::ostream overloads
- */
-template<class T>
-std::ostream & operator<<(std::ostream & os, const std::vector<T> & vec) {
-    os << "[";
-    for (size_t i = 0; i < vec.size(); ++i) {
-            os << vec.at(i);
-            if (i < vec.size() - 1) {
-            os << ", ";
-        }
-    }
-    return os << "]";
-}
-
-template<class K, class V>
-std::ostream & operator<<(std::ostream & os, const std::map<K, V> & map) {
-    os << "{";
-    for (auto it = map.begin(); it != map.end(); it++) {
-        os << it->first << ": " << it->second;
-        if (it != std::prev(map.end())) {
-            os << ", ";
-        }
-    }
-    return os << "}";
-}
-
-template<class K, class V>
-inline std::ostream & operator<<(std::ostream & os, const std::unordered_map<K, V> & map) {
-    os << "{";
-    size_t i = 0;
-    for (const auto & el : map) {
-        os << el.first << ": " << el.second;
-        if (i != map.size()) {
-            os << ", ";
-        }
-        i++;
-    }
-    return os << "}";
-}
-
 template<class ...Args>
 const Logger & Logger::debug(Args && ...args) const {
     return log(Config::LogLevel::Debug, std::forward<Args>(args)...);
--- include/log/utils.h
+++ include/log/utils.h
@@ -90,4 +90,48 @@ namespace jc::log {
     };
 }
 
+// Common data structures std::ostream overloads //
+namespace jc::log {
+    /**
+     * std::ostream overloads
+     */
+    template<class T>
+    std::ostream & operator<<(std::ostream & os, const std::vector<T> & vec) {
+        os << "[";
+        for (size_t i = 0; i < vec.size(); ++i) {
+            os << vec.at(i);
+            if (i < vec.size() - 1) {
+                os << ", ";
+            }
+        }
+        return os << "]";
+    }
+
+    template<class K, class V>
+    std::ostream & operator<<(std::ostream & os, const std::map<K, V> & map) {
+        os << "{";
+        for (auto it = map.begin(); it != map.end(); it++) {
+            os << it->first << ": " << it->second;
+            if (it != std::prev(map.end())) {
+                os << ", ";
+            }
+        }
+        return os << "}";
+    }
+
+    template<class K, class V>
+    inline std::ostream & operator<<(std::ostream & os, const std::unordered_map<K, V> & map) {
+        os << "{";
+        size_t i = 0;
+        for (const auto & el : map) {
+            os << el.first << ": " << el.second;
+            if (i != map.size()) {
+                os << ", ";
+            }
+            i++;
+        }
+        return os << "}";
+    }
+}
+
 #endif // JACY_LOG_UTILS_H
