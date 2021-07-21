2a427d69387ff3aa9246ddba4f65c2fec84bef9f
is_gordeev <gordeev.john@gmail.com>
2021-07-21 21:49:43 +0300

    Add `hir::Party`

---
+++ include/hir/nodes/Party.h
@@ -0,0 +1,12 @@
+#ifndef JACY_HIR_NODES_PARTY_H
+#define JACY_HIR_NODES_PARTY_H
+
+#include "hir/nodes/items.h"
+
+namespace jc::hir {
+    struct Party {
+        Mod rootMod;
+    };
+}
+
+#endif // JACY_HIR_NODES_PARTY_H
