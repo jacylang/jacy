8db5997d9ab852ec9f54f07f45a974c9602bc4f1
is_gordeev <gordeev.john@gmail.com>
2021-07-23 23:12:44 +0300

    Add `hir::Slice`

--- include/hir/nodes/types.h
+++ include/hir/nodes/types.h
@@ -18,6 +18,13 @@ namespace jc::hir {
         type_list inputs;
         type_ptr ret;
     };
+
+    struct Slice : Type {
+        Slice(type_ptr && type, const HirId & hirId, const Span & span)
+            : Type(TypeKind::Slice, hirId, span), type(std::move(type)) {}
+
+        type_ptr type;
+    };
 }
 
 #endif // JACY_HIR_NODES_TYPES_H
