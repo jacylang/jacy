a379d84487fde84479e262cf2b7a62b33586d63e
is_gordeev <gordeev.john@gmail.com>
2021-07-23 23:04:50 +0300

    Introduce `hir::Type`

---
+++ include/hir/nodes/Type.h
@@ -0,0 +1,16 @@
+#ifndef JACY_HIR_NODES_TYPE_H
+#define JACY_HIR_NODES_TYPE_H
+
+#include "hir/nodes/HirNode.h"
+
+namespace jc::hir {
+    enum class TypeKind {
+
+    };
+
+    struct Type : HirNode {
+
+    };
+}
+
+#endif // JACY_HIR_NODES_TYPE_H
