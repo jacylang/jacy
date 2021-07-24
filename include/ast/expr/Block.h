52e3b2ca01f34bc2750cdb0da81a8fb28137c583
is_gordeev <gordeev.john@gmail.com>
2021-07-24 23:29:14 +0300

    Update `ast::Block`

--- include/ast/expr/Block.h
+++ include/ast/expr/Block.h
@@ -10,25 +10,12 @@ namespace jc::ast {
     using opt_block_ptr = Option<block_ptr>;
     using block_list = std::vector<block_ptr>;
 
-    enum class BlockKind {
-        OneLine,
-        Raw,
-    };
-
     struct Block : Expr {
-        Block(expr_ptr && expr, const Span & span)
-            : Expr(span, ExprKind::Block),
-              blockKind(BlockKind::OneLine),
-              oneLine(std::move(expr)) {}
-
         Block(stmt_list && stmts, const Span & span)
             : Expr(span, ExprKind::Block),
-              blockKind(BlockKind::Raw),
               stmts(std::move(stmts)) {}
 
-        BlockKind blockKind;
-        opt_expr_ptr oneLine{None};
-        Option<stmt_list> stmts{None};
+        stmt_list stmts;
 
         void accept(BaseVisitor & visitor) const override {
             return visitor.visit(*this);
