48e72edad84fab27758c2960082f8e9457e92152
hazer-hazer <gordeev.john@gmail.com>
2021-08-02 18:06:24 +0300

    Move diagnostics from `Interface` to `Session`

--- include/core/Interface.h
+++ include/core/Interface.h
@@ -26,109 +26,6 @@
 namespace jc::core {
     using common::Config;
 
-    const auto bench = std::chrono::high_resolution_clock::now;
-
-    enum class MeasUnit {
-        NA,
-        Char,
-        Token,
-        Node,
-        Def,
-    };
-
-    class Step : public std::enable_shared_from_this<Step> {
-    public:
-        using ptr = std::shared_ptr<Step>;
-        using milli_ratio = std::ratio<1, 1000>;
-        using bench_t = std::chrono::time_point<std::chrono::high_resolution_clock>;
-
-    public:
-        Step(
-            Option<ptr> parent,
-            const std::string & name,
-            MeasUnit measUnit
-        ) : parent(parent),
-            name(name),
-            measUnit(measUnit),
-            benchStart(bench()) {}
-
-    public:
-        template<class ...Args>
-        ptr addChild(Args && ...args) {
-            children.emplace_back(std::make_shared<Step>(shared_from_this(), std::forward<Args>(args)...));
-            return children.back();
-        }
-
-        MeasUnit getUnit() const {
-            return measUnit;
-        }
-
-        const std::string & getName() const {
-            return name;
-        }
-
-        auto childrenCount() const {
-            return children.size();
-        }
-
-        Option<ptr> getParent() const {
-            return parent;
-        }
-
-        double getBenchmark() const {
-            return benchmark.unwrap("`Step::getBenchmark`");
-        }
-
-        auto getUnitCount() const {
-            return procUnitCount.unwrap("`Step::getUnitCount`");
-        }
-
-        const auto & getChildren() const {
-            return children;
-        }
-
-        auto isComplete() const {
-            return complete;
-        }
-
-        ptr end(Option<size_t> procUnitCount) {
-            this->procUnitCount = procUnitCount;
-            benchmark = std::chrono::duration<double, milli_ratio>(bench() - benchStart).count();
-            complete = true;
-            return parent.unwrap("`Step::end`");
-        }
-
-        void endFailed() {
-            log::Logger::devDebug("End as failed '", name, "'");
-            procUnitCount = None;
-            benchmark = std::chrono::duration<double, milli_ratio>(bench() - benchStart).count();
-        }
-
-        constexpr const char * unitStr() const {
-            switch (measUnit) {
-                case MeasUnit::Node: return "node";
-                case MeasUnit::Char: return "char";
-                case MeasUnit::Token: return "token";
-                case MeasUnit::Def: return "def";
-                case MeasUnit::NA: return "N/A";
-            }
-        }
-
-    private:
-        Option<ptr> parent{None};
-        std::vector<ptr> children{};
-
-        std::string name;
-
-        MeasUnit measUnit;
-        Option<size_t> procUnitCount{None};
-
-        bench_t benchStart;
-        Option<double> benchmark{None};
-
-        bool complete{false};
-    };
-
     struct FSEntry;
     using fs_entry_ptr = std::shared_ptr<FSEntry>;
     struct FSEntry {
@@ -207,15 +104,6 @@ namespace jc::core {
         sugg::sugg_list suggestions;
         void collectSuggestions(sugg::sugg_list && additional);
         void checkSuggestions(const std::string & stageName);
-
-        // Debug info //
-    private:
-        Step::ptr step;
-        void beginStep(const std::string & name, MeasUnit measUnit);
-        void endStep(Option<size_t> procUnitCount = None);
-
-        void printSteps() noexcept;
-        void printStepsDevMode();
     };
 }
 
--- src/session/Session.cpp
+++ src/session/Session.cpp
@@ -1,4 +1,147 @@
 #include "session/Session.h"
 
 namespace jc::sess {
+    // Debug info //
+    void Session::beginStep(const std::string & name, MeasUnit measUnit) {
+        step = step->addChild(name, measUnit);
+    }
+
+    void Session::endStep(Option<size_t> procUnitCount) {
+        const auto unit = step->getUnit();
+        if (procUnitCount.none()) {
+            // Check if unit exists globally (e.g. node) and not bound to something specific like file, etc.
+            switch (unit) {
+                case MeasUnit::Node: {
+                    procUnitCount = nodeStorage.size();
+                    break;
+                }
+                case MeasUnit::Def: {
+                    procUnitCount = defStorage.size();
+                    break;
+                }
+                case MeasUnit::NA: {
+                    // Don't use this value, check for `MeasUnit::NA`
+                    procUnitCount = 0;
+                    break;
+                }
+                default: {
+                    log::Logger::devPanic(
+                        "Called `Interface::endStep` with step containing non-global measurement unit [",
+                        step->unitStr(),
+                        "] without `procUnitCount`");
+                }
+            }
+        }
+
+        step = step->end(procUnitCount.unwrap());
+    }
+
+    void Session::printSteps() noexcept {
+        // Unwind steps if compiler crashed
+        log::Logger::devDebug(
+            "Unwind steps, current step [",
+            step->getName(),
+            "] is ", step->isComplete() ? "complete" : "incomplete",
+            " and its parent is ",
+            step->getParent().some() ? step->getParent().unwrap()->getName() : "None");
+
+        while (true) {
+            for (const auto & child : step->getChildren()) {
+                if (not child->isComplete()) {
+                    child->endFailed();
+                }
+            }
+
+            if (not step->isComplete()) {
+                step->endFailed();
+            }
+
+            if (step->getParent().some()) {
+                step = step->getParent().unwrap();
+            } else {
+                break;
+            }
+        }
+
+        printStepsDevMode();
+
+        // Print final benchmark
+        log::Logger::print(
+            "Full compilation done in ",
+            step->getBenchmark(),
+            "ms"
+        );
+        log::Logger::nl();
+    }
+
+    void Session::printStepsDevMode() {
+        if (not common::Config::getInstance().checkDev()) {
+            return;
+        }
+
+        // Table
+        // | Benchmark name | Processed entity (e.g. AST) | time | speed
+        // Wrap it to ~120 chars (limit was found by typing), so the layout is following
+        // | 55 | 20 | 15 | 30 (there can be pretty long entity names) |
+        // Note: Choose the shortest names for benchmarks!!!
+
+        constexpr uint8_t BNK_NAME_WRAP_LEN = 60;
+        constexpr uint8_t ENTITY_NAME_WRAP_LEN = 20;
+        constexpr uint8_t TIME_WRAP_LEN = 15;
+        constexpr uint8_t SPEED_WRAP_LEN = 25;
+
+        log::Table<4> table {
+            {BNK_NAME_WRAP_LEN, ENTITY_NAME_WRAP_LEN, TIME_WRAP_LEN, SPEED_WRAP_LEN},
+            {log::Align::Left, log::Align::Center, log::Align::Center, log::Align::Center}
+        };
+
+        std::function<void(const Step::ptr&, uint8_t)> printStep;
+
+        printStep = [&table, &printStep, this](const Step::ptr & step, uint8_t depth) -> void {
+            for (const auto & child : step->getChildren()) {
+                printStep(child, depth + 1);
+            }
+
+            if (depth == 0) {
+                return;
+            }
+
+            const auto time = std::to_string(step->getBenchmark()) + "ms";
+            std::string speed = "N/A";
+            if (step->getUnit() != MeasUnit::NA and step->isComplete()) {
+                speed =
+                    std::to_string(static_cast<double>(step->getUnitCount()) / step->getBenchmark()) + " " + step->unitStr() +
+                    "/ms";
+            }
+
+            std::string preparedName;
+
+            if (depth > 0) {
+                preparedName += utils::str::repeat(" ", depth - 1) + "> ";
+            } else {
+                table.addLine(true);
+            }
+
+            preparedName += step->getName();
+
+            if (not step->isComplete()) {
+                preparedName += " 🔥";
+            }
+
+            table.addRow(preparedName, step->unitStr(), time, speed);
+
+            if (depth == 0 or depth == 1) {
+                table.addLine(true);
+            }
+        };
+
+        table.addSectionName("Summary");
+        table.addHeader("Name", "Entity", "Time", "Speed");
+
+        printStep(step, 0);
+
+        table.addLine(true);
+
+        log::Logger::print(table);
+    }
 }
