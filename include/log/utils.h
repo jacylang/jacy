a2641879792a91f5dd73349cc0542f426f57d4fb
is_gordeev <gordeev.john@gmail.com>
2021-08-11 23:16:57 +0300

    Add `log::Move` and cursor movements

--- include/log/utils.h
+++ include/log/utils.h
@@ -420,6 +420,67 @@ namespace jc::log {
     };
 }
 
+// Cursor //
+namespace jc::log {
+    /// Cursor movement offset
+    /// Left - (-x), Right - (+x), Down - (+y), Up - (-y)
+    ///           ^
+    ///         (-y)
+    ///          |
+    /// <- (-x) -|- (+x) ->
+    ///          |
+    ///        (+y)
+    ///         v
+    struct Move {
+        using dist_t = int16_t;
+
+        dist_t x;
+        dist_t y;
+    };
+
+    #ifdef WIN
+    static inline winMove(Move::dist_t x, Move::dist_t y) {
+        auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
+        if (not handle) {
+            return;
+        }
+
+        CONSOLE_SCREEN_BUFFER_INFO csbfi;
+        GetConsoleScreenBufferInfo(hStdout, &csbiInfo);
+
+        COORD cursor;
+
+        cursor.X = csbfi.dwCursorPosition.X + x;
+        cursor.Y = csbfi.dwCursorPosition.Y + y;
+        SetConsoleCursorPosition(hStdout, cursor);
+    }
+    #endif
+
+    static inline std::ostream & operator<<(std::ostream & os, const Move & move) {
+        #ifdef WIN
+
+        winMove(move.x, move.y);
+
+        #else
+
+        if (move.x > 0) {
+            os << "\033[" << move.x << "C";
+        } else if (move.x < 0) {
+            os << "\033[" << move.x * -1 << "D";
+        }
+
+        if (move.y > 0) {
+            os << "\033[" << move.y << "B";
+        } else if (move.y < 0) {
+            os << "\033[" << move.y * -1 << "A";
+        }
+
+        #endif
+
+        return os;
+    }
+}
+
 #include "log/utils.inl"
 
 #endif // JACY_LOG_UTILS_H
