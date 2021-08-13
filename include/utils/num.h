62475a47974e63b3d40c08d2de8b9b7d5d239404
is_gordeev <gordeev.john@gmail.com>
2021-08-14 01:39:57 +0300

    Add `jc::utils::num::safeAs`

---
+++ include/utils/num.h
@@ -0,0 +1,13 @@
+#ifndef JACY_UTILS_NUM_H
+#define JACY_UTILS_NUM_H
+
+#include <type_traits>
+
+namespace jc::utils::num {
+    template<class T, class F, typename std::enable_if<std::is_integral<F>::value>::type>
+    static inline T safeAs(F i) noexcept (sizeof(F) >= sizeof(T)) {
+        return static_cast<T>(i);
+    }
+}
+
+#endif // JACY_UTILS_NUM_H
