4719997ccd727f669d046098fe072a17dda5430e
is_gordeev <gordeev.john@gmail.com>
2021-08-18 00:28:22 +0300

    Add `cli::Help` command

---
+++ include/cli/commands/Help.h
@@ -0,0 +1,13 @@
+#ifndef JACY_CLI_COMMANDS_HELP_H
+#define JACY_CLI_COMMANDS_HELP_H
+
+#include "cli/commands/BaseCommand.h"
+
+namespace jc::cli {
+    class Help : public BaseCommand {
+    public:
+
+    };
+}
+
+#endif // JACY_CLI_COMMANDS_HELP_H
