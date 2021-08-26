64aa5c5e1dd3c106863a46bdbe8eaa501dd70b7b
is_gordeev <gordeev.john@gmail.com>
2021-08-27 00:14:34 +0300

    Add `utils::num::DistinctInt`

--- include/utils/num.h
+++ include/utils/num.h
@@ -4,8 +4,14 @@
 #include <type_traits>
 
 namespace jc::utils::num {
+    template<class IntT, typename = typename std::enable_if<std::is_integral<IntT>::value>::type>
+    class DistinctInt {
+        DistinctInt(IntT val)  {}
+        IntT val;
+    };
+
     template<class T, class F, typename = typename std::enable_if<std::is_integral<F>::value>::type>
-    static inline T safeAs(F i) noexcept (sizeof(F) >= sizeof(T)) {
+    static inline T safeAs(F i) noexcept(sizeof(F) >= sizeof(T)) {
         return static_cast<T>(i);
     }
 }
