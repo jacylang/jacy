9d4dbb316c510c727681dab819d3680bfe9b1e5c
is_gordeev <gordeev.john@gmail.com>
2021-08-27 20:28:50 +0300

    Add `ast::Init` item

--- include/ast/item/Init.h
+++ include/ast/item/Init.h
@@ -2,10 +2,23 @@
 #define JACY_AST_ITEM_INIT_H
 
 #include "ast/item/Item.h"
+#include "ast/fragments/func_fragments.h"
 
 namespace jc::ast {
     struct Init : Item {
+        Init(
+            FuncSig && sig,
+            Ident::PR name,
+            Option<Body> && body,
+            const Span & span
+        ) : Item{span, ItemKind::Init},
+            sig{std::move(sig)},
+            name{std::move(name)},
+            body{std::move(body)} {}
 
+        FuncSig sig;
+        Ident::PR name;
+        Option<Body> body;
     };
 }
 
