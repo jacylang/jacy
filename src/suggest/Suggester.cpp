5a56a3a53bf87c5b26df41d45049666c4c406fbd
is_gordeev <gordeev.john@gmail.com>
2021-08-29 22:45:11 +0300

    Update `Suggester::pointMsgTo` source path line

--- src/suggest/Suggester.cpp
+++ src/suggest/Suggester.cpp
@@ -63,7 +63,8 @@ namespace jc::sugg {
 
         // Print suggestion source file path
         const auto & spanFilePath = sess->sourceMap.getSourceFile(fileId).path;
-        printWithIndent("   ", "> " + spanFilePath.string());
+        // TODO!: Add dynamic indentation for file path line
+        printWithIndent("> " + spanFilePath.string());
 
         const auto & indent = getFileIndent(fileId);
         // TODO!: Maybe not printing previous line if it's empty?
