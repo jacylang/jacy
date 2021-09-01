bda24d1630ba3db4896bc24972e65a8ef5d053ef
is_gordeev <gordeev.john@gmail.com>
2021-09-01 23:02:02 +0300

    Add `Postfix` ast node

---
+++ include/ast/expr/Postfix.h
@@ -0,0 +1,20 @@
+#ifndef JACY_AST_EXPR_POSTFIX_H
+#define JACY_AST_EXPR_POSTFIX_H
+
+#include "ast/expr/Expr.h"
+
+namespace jc::ast {
+    struct Prefix : Expr {
+        Prefix(Expr::Ptr && lhs, const parser::Token & op, const Span & span)
+            : Expr{span, ExprKind::Prefix}, lhs{std::move(lhs)}, op{op} {}
+
+        Expr::Ptr lhs;
+        parser::Token op;
+
+        void accept(BaseVisitor & visitor) const override {
+            return visitor.visit(*this);
+        }
+    };
+}
+
+#endif // JACY_AST_EXPR_POSTFIX_H
