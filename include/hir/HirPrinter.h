e7d35d7f5609b390f1923303436f4b24fe6f48cf
is_gordeev <gordeev.john@gmail.com>
2021-09-05 00:19:41 +0300

    Add `HirPrinter`

---
+++ include/hir/HirPrinter.h
@@ -0,0 +1,13 @@
+#ifndef JACY_HIR_HIRPRINTER_H
+#define JACY_HIR_HIRPRINTER_H
+
+namespace jc::hir {
+    class HirPrinter {
+    public:
+        HirPrinter() = default;
+
+
+    };
+}
+
+#endif // JACY_HIR_HIRPRINTER_H
