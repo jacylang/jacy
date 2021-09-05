b20d9678ccb368b9e832bb7e5e6e272f1a03fd9c
is_gordeev <gordeev.john@gmail.com>
2021-09-05 05:50:14 +0300

    Update `Configer`, initialize `Config` properties

--- src/config/Configer.cpp
+++ src/config/Configer.cpp
@@ -2,49 +2,52 @@
 
 namespace jc::config {
     void Configer::applyCLIArgs(const cli::PassedCommand & args) {
-        rootFile = args.getEntryFile().unwrap();
+        auto & config = Config::getInstance();
+
+        config.rootFile = args.getEntryFile().unwrap();
 
         // Apply bool args //
-        dev = args.checkBoolFlag("dev");
+        config.dev = args.checkBoolFlag("dev");
 
         // Apply key-value args //
 
         // `print`
         args.getFlagValues("print").then([&](const auto & values) {
             for (const auto & val : values) {
-                print.insert(printKinds.at(val));
+                config.print.insert(config.printKinds.at(val));
             }
         });
 
         // `compile-depth`
         args.getFlagSingleValue("compile-depth").then([&](const auto & value) {
-            compileDepth = compileDepthKinds.at(value);
+            config.compileDepth = config.compileDepthKinds.at(value);
         });
 
         // `benchmark`
         args.getFlagSingleValue("benchmark").then([&](const auto & value) {
-            benchmark = benchmarkKinds.at(value);
+            config.benchmark = config.benchmarkKinds.at(value);
         });
 
         // `parser-extra-debug`
         args.getFlagSingleValue("parser-extra-debug").then([&](const auto & value) {
-            parserExtraDebug = parserExtraDebugKinds.at(value);
+            config.parserExtraDebug = config.parserExtraDebugKinds.at(value);
         });
 
         // `log-level`
         args.getFlagSingleValue("log-level").then([&](const auto & value) {
-            loggerLevels[GLOBAL_LOG_LEVEL_NAME] = loggerLevels.at(value);
+            config.loggerLevels[config.GLOBAL_LOG_LEVEL_NAME] = config.loggerLevels.at(value);
         }).otherwise([&]() {
-            loggerLevels[GLOBAL_LOG_LEVEL_NAME] = dev ? log::LogLevel::Dev : DEFAULT_LOG_LEVEL;
+            config.loggerLevels[config.GLOBAL_LOG_LEVEL_NAME] = config.dev ? log::LogLevel::Dev
+                                                                           : config.DEFAULT_LOG_LEVEL;
         });
 
         // `*-log-level`
-        for (const auto & owner : loggerOwners) {
+        for (const auto & owner : config.loggerOwners) {
             const auto & argName = owner + "-log-level";
             args.getFlagSingleValue(argName).then([&](const auto & value) {
-                loggerLevels[owner] = logLevelKinds.at(value);
+                config.loggerLevels[owner] = config.logLevelKinds.at(value);
             }).otherwise([&]() {
-                loggerLevels[owner] = loggerLevels.at(GLOBAL_LOG_LEVEL_NAME);
+                config.loggerLevels[owner] = config.loggerLevels.at(config.GLOBAL_LOG_LEVEL_NAME);
             });
         }
     }
