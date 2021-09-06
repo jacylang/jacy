b9d4813b5a15e443c24600c9bdf1407002cd43ae
is_gordeev <gordeev.john@gmail.com>
2021-09-06 21:41:59 +0300

    Add `span::Kw`

--- include/span/Symbol.h
+++ include/span/Symbol.h
@@ -6,12 +6,60 @@
 #include <vector>
 
 namespace jc::span {
+    /// Keywords
+    enum class KW {
+        And,
+        As,
+        Async,
+        Await,
+        Break,
+        Const,
+        Continue,
+        Do,
+        Elif,
+        Else,
+        Enum,
+        False,
+        For,
+        Func,
+        If,
+        Impl,
+        Import,
+        In,
+        Infix,
+        Init,
+        Loop,
+        Match,
+        Mod,
+        Move,
+        Mut,
+        Not,
+        Of,
+        Or,
+        Return,
+        Party,
+        Pub,
+        Ref,
+        Self,
+        Static,
+        Struct,
+        Super,
+        This,
+        Trait,
+        True,
+        Type,
+        Use,
+        Let,
+        Where,
+        While,
+    };
+
     struct SymbolId {
         using ValueT = uint32_t;
 
         ValueT val;
     };
-    
+
     struct Symbol {
         using Opt = Option<Symbol>;
         using SymMap = std::map<std::string, Symbol>;
@@ -21,7 +69,8 @@ namespace jc::span {
 
     class Interner {
     public:
-        Interner() = default;
+        Interner() {
+        }
 
         Symbol intern(const std::string & str) {
             const auto & found = symbols.find(str);
@@ -42,7 +91,10 @@ namespace jc::span {
         }
 
     private:
+        /// Maps Symbol name to its value
         Symbol::SymMap symbols;
+
+        /// Stores all interned strings, SymbolId points to its index
         std::vector<std::string> internedStrings;
     };
 }
