c12c6d4cb67442ed0c5b1a1903a3b62016084510
is_gordeev <gordeev.john@gmail.com>
2021-09-09 00:43:51 +0300

    Update `NameResolver::resolvePath`, add suffix for last path segment

--- src/resolve/NameResolver.cpp
+++ src/resolve/NameResolver.cpp
@@ -362,6 +362,11 @@ namespace jc::resolve {
             const auto & seg = path.segments.at(i).unwrap();
             auto segName = seg.ident.unwrap().sym;
 
+            // Add suffix for last segment if present
+            if (not isPrefixSeg and suffix.some()) {
+                segName += suffix.unwrap();
+            }
+
             // TODO: Resolve segment generics
 
             searchMod->find(ns, segName).then([&](const DefId & defId) {
