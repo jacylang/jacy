e27fc99d72b773564e6724c39654364c36e15fe6
is_gordeev <gordeev.john@gmail.com>
2021-09-27 00:13:22 +0300

    Add `TrueColor::fromHex3`

--- include/log/data_types.h
+++ include/log/data_types.h
@@ -75,7 +75,8 @@ namespace jc::log {
         TrueColor(uint8_t r, uint8_t g, uint8_t b) : r{r}, g{g}, b{b} {}
         TrueColor(const std::string & str);
 
-        static TrueColor fromHex(uint32_t value);
+        static TrueColor fromHex6(uint32_t value);
+        static TrueColor fromHex3(uint16_t value);
 
         uint8_t r;
         uint8_t g;
--- src/log/data_types.cpp
+++ src/log/data_types.cpp
@@ -2,11 +2,20 @@
 #include "utils/str.h"
 
 namespace jc::log {
-    TrueColor TrueColor::fromHex(uint32_t value) {
+
+    TrueColor TrueColor::fromHex6(uint32_t value) {
         return TrueColor {
             static_cast<uint8_t>(((value >> 16) & 0xFF)),
             static_cast<uint8_t>(((value >> 8) & 0xFF)),
             static_cast<uint8_t>((value & 0xFF))
+        };
+    }
+
+    TrueColor TrueColor::fromHex3(uint16_t value) {
+        return TrueColor {
+            static_cast<uint8_t>((value >> 8) & 0xF),
+            static_cast<uint8_t>((value >> 4) & 0xF),
+            static_cast<uint8_t>(value & 0xF)
         };
     }
 }
