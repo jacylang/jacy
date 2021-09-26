bcf34700fe14d8f1c22cc12a5688d08858093aa7
is_gordeev <gordeev.john@gmail.com>
2021-09-26 17:58:02 +0300

    Rename `Highlighter`

---
+++ include/suggest/Highlighter.h
@@ -0,0 +1,34 @@
+#ifndef JACY_SUGGEST_HIGHLIGHT_H
+#define JACY_SUGGEST_HIGHLIGHT_H
+
+#include "log/data_types.h"
+#include "parser/Lexer.h"
+
+namespace jc::sugg {
+    using log::TrueColor;
+
+    struct Theme {
+        TrueColor text{248, 248, 242}; // Raw text, e.g. variable
+        TrueColor comment{98, 114, 164}; // Line or Block comment
+        TrueColor lit{189, 147, 249}; // Literal
+        TrueColor kw{255, 121, 198}; // Keyword
+        TrueColor op{255, 121, 198}; // Operator
+        TrueColor type{139, 233, 253}; // Type color
+        TrueColor func{80, 250, 123}; // Type color
+    };
+
+    class Highlight {
+    public:
+        Highlight() = default;
+        ~Highlight() = default;
+
+        std::string highlight(const std::string & source);
+
+        TrueColor getTokColor(const parser::Token & tok, const parser::Token::Opt & nextTok);
+
+    private:
+        Theme theme;
+    };
+}
+
+#endif // JACY_SUGGEST_HIGHLIGHT_H
---
+++ src/suggest/Highlighter.cpp
@@ -0,0 +1,53 @@
+#include "suggest/Highlight.h"
+
+namespace jc::sugg {
+    std::string Highlight::highlight(const std::string & source) {
+        parser::Lexer lexer;
+        auto tokens = lexer.lexInternal(source);
+
+        std::stringstream result;
+
+        for (size_t i = 0; i < tokens.size(); i++) {
+            const auto & tok = tokens.at(i);
+            parser::Token::Opt nextToken = None;
+            if (i < tokens.size() - 1) {
+                nextToken = tokens.at(i + 1);
+            }
+            result << getTokColor(tok, nextToken) << tok << log::Color::Reset;
+        }
+
+        return result.str();
+    }
+
+    TrueColor Highlight::getTokColor(const parser::Token & tok, const parser::Token::Opt & nextTok) {
+        if (tok.isLiteral()) {
+            return theme.lit;
+        }
+
+        if (tok.is(parser::TokenKind::Id)) {
+            if (tok.isSomeKeyword()) {
+                return theme.kw;
+            }
+            const auto & ident = tok.asSymbol().toString();
+            if (ident.at(0) >= 'A' and ident.at(0) <= 'Z') {
+                return theme.type;
+            }
+
+            if (nextTok.some() and nextTok.unwrap().is(parser::TokenKind::LParen)) {
+                return theme.func;
+            }
+
+            return theme.text;
+        }
+
+        if (tok.isSomeOp()) {
+            return theme.op;
+        }
+
+        if (tok.is(parser::TokenKind::LineComment) or tok.is(parser::TokenKind::BlockComment)) {
+            return theme.comment;
+        }
+
+        return theme.text;
+    }
+}
