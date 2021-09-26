3f77d0156e069b6c4854dcb6074ee3afd1caf8f5
is_gordeev <gordeev.john@gmail.com>
2021-09-26 23:30:09 +0300

    Update `TrueColor`

--- include/log/data_types.h
+++ include/log/data_types.h
@@ -72,6 +72,8 @@ namespace jc::log {
             Background,
         };
 
+        TrueColor(uint8_t r, uint8_t g, uint8_t b) : r{r}, g{g}, b{b} {}
+
         uint8_t r;
         uint8_t g;
         uint8_t b;
