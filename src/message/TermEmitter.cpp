00b68452d1587c3a75afa79092b2405c1d568fe4
is_gordeev <gordeev.john@gmail.com>
2021-10-06 00:05:06 +0300

    Add `TermEmitter::getFileIndent`

--- src/message/TermEmitter.cpp
+++ src/message/TermEmitter.cpp
@@ -1,6 +1,8 @@
 #include "message/TermEmitter.h"
 
 namespace jc::message {
+    using namespace utils::str;
+
     TermEmitter::TermEmitter() = default;
 
     void TermEmitter::emit(const sess::Session::Ptr & sess, const Message::List & messages) {
@@ -21,5 +23,18 @@ namespace jc::message {
 
     void TermEmitter::printWithIndent(FileId fileId, const std::string & text) {
 
+    }
+
+    TermEmitter::Indent TermEmitter::getFileIndent(FileId fileId) {
+        const auto & found = indentsCache.find(fileId);
+
+        if (found != indentsCache.end()) {
+            return found->second;
+        }
+
+        auto lastLineNum = sess->sourceMap.getLinesCount(fileId);
+        auto indent = Indent {lastLineNum + 3};
+        indentsCache.emplace(fileId, indent);
+        return indent;
     }
 }
