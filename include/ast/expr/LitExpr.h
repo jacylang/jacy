bf1b22bf3c13c3948c9faa9b843b133e2d291a22
is_gordeev <gordeev.john@gmail.com>
2021-10-11 23:29:32 +0300

    Update `ast::LitExpr`, use ADT and pre-evaluated value storage

--- include/ast/expr/LitExpr.h
+++ include/ast/expr/LitExpr.h
@@ -5,10 +5,71 @@
 
 namespace jc::ast {
     struct LitExpr : Expr {
-        explicit LitExpr(Expr::Ptr && expr, const Span & span)
-            : Expr {span, ExprKind::LiteralConstant}, expr {std::move(expr)} {}
+        struct Bool {
+            using ValueT = bool;
 
-        Expr::Ptr expr;
+            ValueT val;
+        };
+
+        struct Int {
+            using ValueT = uint64_t;
+
+            enum class Kind {
+                Unset,
+
+                U8,
+                U16,
+                U32,
+                U64,
+                Uint,
+                I8,
+                I16,
+                I32,
+                I64,
+                Int,
+            } kind;
+            ValueT val;
+        };
+
+        struct Float {
+            enum class Kind {
+                Unset,
+
+                F32,
+                F64,
+            };
+
+            /// Symbol of the float value. Not storing actual value as double to avoid any problems,
+            /// I'm afraid of problems with float
+            span::Symbol sym;
+        };
+
+        struct Str {
+            span::Symbol sym;
+        };
+
+        using ValueT = std::variant<Bool, Int, Float, Str>;
+
+        enum class Kind {
+            Bool,
+            Int,
+            Float,
+            Str,
+        };
+
+    private:
+        LitExpr(ValueT val, const parser::Token & token)
+            : Expr {token.span, ExprKind::LiteralConstant}, token {token}, val {val} {}
+
+    public:
+        LitExpr fromToken(const parser::Token & tok) {
+
+        }
+
+    public:
+        parser::Token token;
+        Kind kind;
+        ValueT val;
 
         void accept(BaseVisitor & visitor) const override {
             return visitor.visit(*this);
