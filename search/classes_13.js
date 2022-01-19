af51022b8046f3499be3ce60e6294a5d61e755f0
hazer-hazer <hazer-hazer@users.noreply.github.com>
2022-01-19 21:48:01 +0000

    deploy: 45a36000aeff11ceb2c8b8527651641326c5774b

--- search/classes_13.js
+++ search/classes_13.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['validator_0',['Validator',['../classjc_1_1ast_1_1_validator.html',1,'jc::ast']]],
-  ['variant_1',['Variant',['../structjc_1_1hir_1_1_variant.html',1,'jc::hir']]],
+  ['variant_1',['Variant',['../structjc_1_1ast_1_1_variant.html',1,'jc::ast::Variant'],['../structjc_1_1hir_1_1_variant.html',1,'jc::hir::Variant']]],
   ['vis_2',['Vis',['../structjc_1_1ast_1_1_vis.html',1,'jc::ast']]]
 ];
