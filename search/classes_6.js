6a27ec18c28bc319b3f3ccde6ed68eb7bd447a99
hazer-hazer <hazer-hazer@users.noreply.github.com>
2022-02-02 21:51:26 +0000

    deploy: 14a33e07ffe76031664921877d4e0191c66cbcc2

--- search/classes_6.js
+++ search/classes_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['genericparam_0',['GenericParam',['../structjc_1_1ast_1_1_generic_param.html',1,'jc::ast::GenericParam'],['../structjc_1_1hir_1_1_generic_param.html',1,'jc::hir::GenericParam']]]
+  ['genericarg_0',['GenericArg',['../structjc_1_1ast_1_1_generic_arg.html',1,'jc::ast::GenericArg'],['../structjc_1_1hir_1_1_generic_arg.html',1,'jc::hir::GenericArg']]],
+  ['genericparam_1',['GenericParam',['../structjc_1_1ast_1_1_generic_param.html',1,'jc::ast::GenericParam'],['../structjc_1_1hir_1_1_generic_param.html',1,'jc::hir::GenericParam']]]
 ];
