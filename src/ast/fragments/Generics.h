5f46caf36f97c9bee214a3ff71a2ddc44f3c2818
hazer-hazer <gordeev.john@gmail.com>
2022-02-04 00:00:41 +0300

    Update `ast::TypeParam`, `ast::ConstParam`, `ast::Lifetime` and make `GenericParam` adt container

--- src/ast/fragments/Generics.h
+++ src/ast/fragments/Generics.h
@@ -14,68 +14,55 @@ namespace jc::ast {
         Const,
     };
 
-    struct GenericParam : Node {
-        using List = std::vector<N<GenericParam>>;
-        using OptList = Option<List>;
-
-        explicit GenericParam(GenericParamKind kind, Span span) : Node{span}, kind{kind} {}
-        virtual ~GenericParam() = default;
-
-        GenericParamKind kind;
-
-        virtual void accept(BaseVisitor & visitor) const = 0;
-    };
-
-    struct TypeParam : GenericParam {
-        TypeParam(
-            Ident::PR name,
-            Option<GenericsTypePtr> type,
-            Span span
-        ) : GenericParam{GenericParamKind::Type, span},
-            name{std::move(name)},
-            boundType{std::move(type)} {}
-
-        Ident::PR name;
-        Option<PR<N<Type>>> boundType;
-
-        void accept(BaseVisitor & visitor) const override {
-            return visitor.visit(*this);
-        }
-    };
-
     /// Common structure for generic parameters and arguments as being just an identifier
     /// Span of the Lifetime (from `GenericParam <- Node`) is a span of lifetime with quote `'`
     /// whereas `name`'s span does not include it.
-    struct Lifetime : GenericParam {
+    struct Lifetime : Node {
         Lifetime(Ident::PR name, Span span)
-            : GenericParam{GenericParamKind::Lifetime, span},
+            : Node(span),
               name{std::move(name)} {}
 
         Ident::PR name;
-
-        void accept(BaseVisitor & visitor) const override {
-            return visitor.visit(*this);
-        }
+        Span span;
     };
 
-    struct ConstParam : GenericParam {
+    struct ConstParam : Node {
         ConstParam(
             Ident::PR name,
-            PR<N<Type>> type,
+            GenericsTypePtr type,
             Expr::OptPtr defaultValue,
             Span span
-        ) : GenericParam{GenericParamKind::Const, span},
+        ) : Node {span},
             name{std::move(name)},
             type{std::move(type)},
             defaultValue{std::move(defaultValue)} {}
 
         Ident::PR name;
-        PR<N<Type>> type;
+        GenericsTypePtr type;
         Expr::OptPtr defaultValue;
+    };
 
-        void accept(BaseVisitor & visitor) const override {
-            return visitor.visit(*this);
-        }
+    struct TypeParam : Node {
+        TypeParam(
+            Ident::PR name,
+            Option<GenericsTypePtr> type,
+            Span span
+        ) : Node {span},
+            name{std::move(name)},
+            boundType{std::move(type)} {}
+
+        Ident::PR name;
+        Option<GenericsTypePtr> boundType;
+    };
+
+    struct GenericParam : Node {
+        using ValueT = std::variant<TypeParam, ConstParam, Lifetime>;
+        using List = std::vector<N<GenericParam>>;
+        using OptList = Option<List>;
+
+        explicit GenericParam(GenericParamKind kind, Span span) : Node{span}, kind{kind} {}
+
+        GenericParamKind kind;
     };
 
     // Generic arguments //
