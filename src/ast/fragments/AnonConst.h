10138cd90e93ea77af480477e7f49b42ceb96359
hazer-hazer <gordeev.john@gmail.com>
2022-02-08 01:05:45 +0300

    Update `ast::AnonConst`, remove `Node` inheritance

--- src/ast/fragments/AnonConst.h
+++ src/ast/fragments/AnonConst.h
@@ -4,15 +4,15 @@
 #include "ast/expr/Expr.h"
 
 namespace jc::ast {
-    struct AnonConst : Node {
+    struct AnonConst {
         using Opt = Option<AnonConst>;
 
         // TODO: Think how to get rid of useless `Span` from `Node` (`AnonConst` need to be child of `Node`)
 
-        AnonConst(Expr::Ptr && expr)
-            : Node {expr.span()}, expr {std::move(expr)} {}
+        AnonConst(Expr::Ptr && expr) : expr {std::move(expr)} {}
 
         Expr::Ptr expr;
+        NodeId id;
 
         void accept(BaseVisitor & visitor) const {
             return visitor.visit(*this);
