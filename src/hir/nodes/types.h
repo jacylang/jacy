e2d985783742fc968ad96cd9e5ed989983ed10ac
hazer-hazer <gordeev.john@gmail.com>
2022-02-13 22:04:42 +0300

    Update `hir` type nodes

--- src/hir/nodes/types.h
+++ src/hir/nodes/types.h
@@ -6,45 +6,45 @@
 #include "hir/nodes/fragments.h"
 
 namespace jc::hir {
-    struct TupleType : Type {
+    struct TupleType : TypeKind {
         TupleType(Type::List && types, HirId hirId, Span span)
-            : Type {Type::Kind::Tuple, hirId, span}, types {std::move(types)} {}
+            : TypeKind {TypeKind::Kind::Tuple}, types {std::move(types)} {}
 
         Type::List types;
     };
 
-    struct FuncType : Type {
-        FuncType(Type::List && inputs, Type::Ptr && ret, HirId hirId, Span span)
-            : Type {Type::Kind::Func, hirId, span}, inputs {std::move(inputs)}, ret {std::move(ret)} {}
+    struct FuncType : TypeKind {
+        FuncType(Type::List && inputs, Type && ret, HirId hirId, Span span)
+            : TypeKind {TypeKind::Kind::Func}, inputs {std::move(inputs)}, ret {std::move(ret)} {}
 
         Type::List inputs;
-        Type::Ptr ret;
+        Type ret;
     };
 
-    struct SliceType : Type {
-        SliceType(Type::Ptr && type, HirId hirId, Span span)
-            : Type {Type::Kind::Slice, hirId, span}, type {std::move(type)} {}
+    struct SliceType : TypeKind {
+        SliceType(Type && type, HirId hirId, Span span)
+            : TypeKind {TypeKind::Kind::Slice}, type {std::move(type)} {}
 
-        Type::Ptr type;
+        Type type;
     };
 
-    struct ArrayType : Type {
-        ArrayType(Type::Ptr && type, AnonConst && size, HirId hirId, Span span)
-            : Type {Type::Kind::Array, hirId, span}, type {std::move(type)}, size {std::move(size)} {}
+    struct ArrayType : TypeKind {
+        ArrayType(Type && type, AnonConst && size, HirId hirId, Span span)
+            : TypeKind {TypeKind::Kind::Array}, type {std::move(type)}, size {std::move(size)} {}
 
-        Type::Ptr type;
+        Type type;
         AnonConst size;
     };
 
-    struct TypePath : Type {
+    struct TypePath : TypeKind {
         TypePath(Path && path, HirId hirId, Span span)
-            : Type {Type::Kind::Path, hirId, span}, path {std::move(path)} {}
+            : TypeKind {TypeKind::Kind::Path}, path {std::move(path)} {}
 
         Path path;
     };
 
-    struct UnitType : Type {
-        UnitType(HirId hirId, Span span) : Type {Type::Kind::Unit, hirId, span} {}
+    struct UnitType : TypeKind {
+        UnitType(HirId hirId, Span span) : TypeKind {TypeKind::Kind::Unit} {}
     };
 }
 
