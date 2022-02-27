a9b2c2074d2ca91aca8c734368060b9ebea6b39e
hazer-hazer <gordeev.john@gmail.com>
2022-02-27 18:10:36 +0300

    Revert "Update `ast::Item`, add `ast::ItemKind` as we do in `hir`"
    This reverts commit 17c8992300bbd6bf1aa905207b67af5ceaa53a8d.

--- src/ast/item/Item.h
+++ src/ast/item/Item.h
@@ -20,8 +20,9 @@ namespace jc::ast {
         span::Span::Opt span;
     };
 
-    struct ItemKind : Node {
-        using Ptr = PR<N<ItemKind>>;
+    struct Item : Node {
+        using Ptr = PR<N<Item>>;
+        using List = std::vector<Ptr>;
 
         enum class Kind {
             Enum,
@@ -35,34 +36,28 @@ namespace jc::ast {
             Use,
         };
 
-        ItemKind(Kind kind, Span span) : Node {span}, kind {kind} {}
-
-        Kind kind;
-
-        template<class T>
-        static T * as(const N<ItemKind> & item) {
-            return static_cast<T *>(item.get());
-        }
-    };
-
-    template<class KindT = ItemKind>
-    struct Item : Node {
-        using List = std::vector<Ptr>;
-
-        Item(Attr::List && attributes, Vis vis, Ident::PR && name, KindT && kind)
-            : Node {span},
-              attributes {std::move(attributes)},
-              vis {vis}, name {std::move(name)},
-              kind {std::move(kind)} {}
+        Item(Span span, Kind kind) : Node {span}, kind {kind} {}
 
         Attr::List attributes;
+        Kind kind;
         Vis vis;
-        Ident::PR name;
-        KindT kind;
+
+        void setAttributes(Attr::List && attributes) {
+            this->attributes = std::move(attributes);
+        }
+
+        void setVis(Vis && vis) {
+            this->vis = std::move(vis);
+        }
 
         virtual span::Ident getName() const = 0;
 
         virtual NodeId::Opt getNameNodeId() const = 0;
+
+        template<class T>
+        static T * as(const N<Item> & item) {
+            return static_cast<T*>(item.get());
+        }
 
         virtual void accept(BaseVisitor & visitor) const = 0;
     };
