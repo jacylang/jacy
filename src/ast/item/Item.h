a7a8af348a35027af32a819c249811297a3c42de
hazer-hazer <gordeev.john@gmail.com>
2022-02-27 23:22:03 +0300

    Remove `ast::AssocItem`

--- src/ast/item/Item.h
+++ src/ast/item/Item.h
@@ -61,18 +61,6 @@ namespace jc::ast {
 
         virtual void accept(BaseVisitor & visitor) const = 0;
     };
-
-    struct AssocItem : Node {
-        enum class Kind {
-            Const,
-            Func,
-            TypeAlias,
-        };
-
-        Kind kind;
-        Attr::List attributes;
-        Vis vis;
-    };
 }
 
 #endif // JACY_AST_ITEM_ITEM_H
